a475308c0a2d8566af1baf0eb313e8b39323cc52
John Kerl <kerl.john.r@gmail.com>
2015-05-26 07:19:34 -0400

    cut -o 1st pass

--- c/mapping/mapper_cut.c
+++ c/mapping/mapper_cut.c
@@ -5,20 +5,25 @@
 #include "mapping/mappers.h"
 #include "cli/argparse.h"
 
+#define DO_KEEP_REC_ORDER 0xe7
+#define DO_KEEP_ARG_ORDER 0xe8
+#define DO_COMPLEMENT     0xe9
+
 typedef struct _mapper_cut_state_t {
 	slls_t* pfield_name_list;
 	hss_t*  pfield_name_set;
-	int     do_complement;
+	int     do_which;
 } mapper_cut_state_t;
 
 // ----------------------------------------------------------------
 static sllv_t* mapper_cut_process(lrec_t* pinrec, context_t* pctx, void* pvstate) {
 	if (pinrec != NULL) {
 		mapper_cut_state_t* pstate = (mapper_cut_state_t*)pvstate;
-		if (!pstate->do_complement) {
+		if (pstate->do_which == DO_KEEP_REC_ORDER) {
 
-			// xxx loop over the prec & free the ones not in the set ... being careful about
-			// the fact that we're modifying what we're looping over.
+			// Loop over the record and free the fields not in the
+			// to-be-retained set, being careful about the fact that we're
+			// modifying what we're looping over.
 			for (lrece_t* pe = pinrec->phead; pe != NULL; /* next in loop */) {
 				if (!hss_has(pstate->pfield_name_set, pe->key)) {
 					lrece_t* pf = pe->pnext;
@@ -29,7 +34,31 @@ static sllv_t* mapper_cut_process(lrec_t* pinrec, context_t* pctx, void* pvstate
 				}
 			}
 			return sllv_single(pinrec);
-		} else {
+
+		} else if (pstate->do_which == DO_KEEP_ARG_ORDER) {
+
+			// Loop over the record and free the fields not in the
+			// to-be-retained set, being careful about the fact that we're
+			// modifying what we're looping over.
+			for (lrece_t* pe = pinrec->phead; pe != NULL; /* next in loop */) {
+				if (!hss_has(pstate->pfield_name_set, pe->key)) {
+					lrece_t* pf = pe->pnext;
+					lrec_remove(pinrec, pe->key);
+					pe = pf;
+				} else {
+					pe = pe->pnext;
+				}
+			}
+
+			// OK since the field-name list was reversed at construction time.
+			for (sllse_t* pe = pstate->pfield_name_list->phead; pe != NULL; pe = pe->pnext) {
+				char* field_name = pe->value;
+				lrec_move_to_head(pinrec, field_name);
+			}
+
+			return sllv_single(pinrec);
+
+		} else { // DO_COMPLEMENT
 			for (sllse_t* pe = pstate->pfield_name_list->phead; pe != NULL; pe = pe->pnext) {
 				char* field_name = pe->value;
 				lrec_remove(pinrec, field_name);
@@ -51,15 +80,16 @@ static void mapper_cut_free(void* pvstate) {
 		hss_free(pstate->pfield_name_set);
 }
 
-static mapper_t* mapper_cut_alloc(slls_t* pfield_name_list, int do_complement) {
+static mapper_t* mapper_cut_alloc(slls_t* pfield_name_list, int do_which) {
 	mapper_t* pmapper = mlr_malloc_or_die(sizeof(mapper_t));
 
 	mapper_cut_state_t* pstate = mlr_malloc_or_die(sizeof(mapper_cut_state_t));
 	pstate->pfield_name_list   = pfield_name_list;
+	slls_reverse(pstate->pfield_name_list);
 	pstate->pfield_name_set    = hss_alloc();
 	for (sllse_t* pe = pfield_name_list->phead; pe != NULL; pe = pe->pnext)
 		hss_add(pstate->pfield_name_set, pe->value);
-	pstate->do_complement = do_complement;
+	pstate->do_which = do_which;
 
 	pmapper->pvstate       = (void*)pstate;
 	pmapper->pprocess_func = mapper_cut_process;
@@ -71,7 +101,9 @@ static mapper_t* mapper_cut_alloc(slls_t* pfield_name_list, int do_complement) {
 // ----------------------------------------------------------------
 static void mapper_cut_usage(char* argv0, char* verb) {
 	fprintf(stdout, "Usage: %s %s [options]\n", argv0, verb);
-	fprintf(stdout, "-f {a,b,c}       Field names to cut.\n");
+	fprintf(stdout, "-f {a,b,c}       Field names to include for cut.\n");
+	fprintf(stdout, "-o               Retain fields in the order specified here in the argument list.\n");
+	fprintf(stdout, "                 Default is to retain them in the order found in the input data.\n");
 	fprintf(stdout, "-x|--complement  Exclude, rather that include, field names specified by -f.\n");
 	fprintf(stdout, "Passes through input records with specified fields included/excluded.\n");
 }
@@ -79,13 +111,15 @@ static void mapper_cut_usage(char* argv0, char* verb) {
 // ----------------------------------------------------------------
 static mapper_t* mapper_cut_parse_cli(int* pargi, int argc, char** argv) {
 	slls_t* pfield_name_list  = NULL;
+	int     do_keep_arg_order = FALSE;
 	int     do_complement     = FALSE;
 
 	char* verb = argv[(*pargi)++];
 
 	ap_state_t* pstate = ap_alloc();
 	ap_define_string_list_flag(pstate, "-f", &pfield_name_list);
-	ap_define_true_flag(pstate, "-x", &do_complement);
+	ap_define_true_flag(pstate, "-o",           &do_keep_arg_order);
+	ap_define_true_flag(pstate, "-x",           &do_complement);
 	ap_define_true_flag(pstate, "--complement", &do_complement);
 
 	if (!ap_parse(pstate, verb, pargi, argc, argv)) {
@@ -98,7 +132,8 @@ static mapper_t* mapper_cut_parse_cli(int* pargi, int argc, char** argv) {
 		return NULL;
 	}
 
-	return mapper_cut_alloc(pfield_name_list, do_complement);
+	int do_which = do_complement ? DO_COMPLEMENT : do_keep_arg_order ? DO_KEEP_ARG_ORDER : DO_KEEP_REC_ORDER;
+	return mapper_cut_alloc(pfield_name_list, do_which);
 }
 
 // ----------------------------------------------------------------
