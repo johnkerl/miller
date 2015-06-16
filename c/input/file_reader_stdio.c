b04daa801122432c5b3174f1a480f19387af1a45
John Kerl <kerl.john.r@gmail.com>
2015-06-15 22:54:17 -0400

    join iterate

---
+++ c/input/file_reader_stdio.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include "lib/mlrutil.h"
+#include "lib/mlr_globals.h"
+#include "file_reader_stdio.h"
+
+void* file_reader_stdio_vopen(char* filename) {
+	FILE* input_stream = stdin;
+
+	if (!streq(filename, "-")) {
+		input_stream = fopen(filename, "r");
+		if (input_stream == NULL) {
+			fprintf(stderr, "%s: Couldn't open \"%s\" for read.\n", MLR_GLOBALS.argv0, filename);
+			perror(filename);
+			return 0;
+		}
+	}
+	return input_stream;
+}
+
+void file_reader_stdio_vclose(void* pvhandle) {
+	FILE* input_stream = pvhandle;
+	if (input_stream != stdin)
+		fclose(input_stream);
+}
---
+++ c/input/file_reader_stdio.h
@@ -0,0 +1,7 @@
+#ifndef FILE_READER_STDIO_H
+#define FILE_READER_STDIO_H
+
+void* file_reader_stdio_vopen(char* file_name);
+void file_reader_stdio_vclose(void* pvhandle);
+
+#endif // FILE_READER_STDIO_H
