ea980a8ac96d5eef1361fcb2527537ea712a6482
jungle-boogie <sean@jungleboogie.me>
2015-08-26 14:25:42 -0700

    changing to sh

--- data/s1.sh
+++ data/s1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 mlr --opprint "$@" stats1 -a mean,sum,count,min,max -f i,x,y -g a,b ../data/medium
 echo
 mlr --opprint "$@" stats1 -a mean,sum,count,min,max -f i,x,y ../data/medium
