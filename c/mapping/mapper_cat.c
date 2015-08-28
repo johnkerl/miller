cf3f979156acad22e9ec2d57cb30411b19ac420a
John Kerl <kerl.john.r@gmail.com>
2015-08-27 22:17:23 -0400

    neaten

--- c/mapping/mapper_cat.c
+++ c/mapping/mapper_cat.c
@@ -4,9 +4,8 @@
 
 // ----------------------------------------------------------------
 static sllv_t* mapper_cat_process(lrec_t* pinrec, context_t* pctx, void* pvstate) {
-	if (pinrec != NULL) {
+	if (pinrec != NULL)
 		return sllv_single(pinrec);
-	}
 	else
 		return sllv_single(NULL);
 }
