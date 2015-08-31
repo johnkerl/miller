b65f52c47831df9e879d3d1235b239bcacc1b2a3
John Kerl <kerl.john.r@gmail.com>
2015-08-31 18:36:40 -0400

    read performance iterate

--- c/bld.sh
+++ c/bld.sh
@@ -1,6 +1,5 @@
 #gcc -Wall -Werror -I.  lib/mlrutil.c lib/string_builder.c lib/mlr_globals.c containers/slls.c input/peek_file_reader.c experimental/csv0.c
-gcc -Wall -Werror -I. -O3 lib/mlrutil.c lib/mlr_globals.c experimental/getline_for_profile.c -o get1
 gcc -Wall -Werror -I. -O3 lib/mlrutil.c lib/mlr_globals.c \
   lib/string_builder.c \
   input/peek_file_reader.c \
-  experimental/getmulti_for_profile.c -o get2
+  experimental/getlines.c -o getl
