4626947cbc8b1ba84349269f9c3fc165d9a2a84e
John Kerl <kerl.john.r@gmail.com>
2015-09-04 18:13:13 -0400

    [read performance iterate] byte-reader iterate

--- c/input/test_byte_readers.c
+++ c/input/test_byte_readers.c
@@ -4,31 +4,17 @@
 #include "lib/minunit.h"
 #include "input/byte_readers.h"
 
-#ifdef __TEST_STDIO_BYTE_READER_MAIN__
+#ifdef __TEST_BYTE_READERS_MAIN__
 int tests_run         = 0;
 int tests_failed      = 0;
 int assertions_run    = 0;
 int assertions_failed = 0;
 
-// xxx mkstemp
-// xxx tmpfile
-// xxx pop from buf
-// xxx take dirname from argv[1]?
-
-// ----------------------------------------------------------------
-static FILE* make_temp_file(char* contents) {
-	xxx
-
-	int fd = mkstemp("/tmp/mlr-ut-XXXXXXXX");
-
-}
-
 // ----------------------------------------------------------------
 static char* test_it() {
-	byte_reader_t* pbr = stdio_byte_reader_alloc();
+	byte_reader_t* pbr = string_byte_reader_alloc();
 
-	char* filename = xxx;
-	int ok = pbr->popen_func(pbr, filename);
+	int ok = pbr->popen_func(pbr, "");
 	mu_assert_lf(ok == TRUE);
 	mu_assert_lf(pbr->pread_func(pbr) == EOF);
 	mu_assert_lf(pbr->pread_func(pbr) == EOF);
@@ -54,6 +40,22 @@ static char* test_it() {
 	return NULL;
 }
 
+#if 0
+// ----------------------------------------------------------------
+// xxx mkstemp
+// xxx tmpfile
+// xxx pop from buf
+// xxx take dirname from argv[1]?
+
+// ----------------------------------------------------------------
+static FILE* make_temp_file(char* contents) {
+	xxx
+
+	int fd = mkstemp("/tmp/mlr-ut-XXXXXXXX");
+
+}
+#endif
+
 // ================================================================
 static char * run_all_tests() {
 	mu_run_test(test_it);
@@ -67,11 +69,11 @@ int main(int argc, char **argv) {
 		printf("Not all unit tests passed\n");
 	}
 	else {
-		printf("TEST_STDIO_BYTE_READER: ALL UNIT TESTS PASSED\n");
+		printf("TEST_BYTE_READERS: ALL UNIT TESTS PASSED\n");
 	}
 	printf("Tests      passed: %d of %d\n", tests_run - tests_failed, tests_run);
 	printf("Assertions passed: %d of %d\n", assertions_run - assertions_failed, assertions_run);
 
 	return result != 0;
 }
-#endif // __TEST_STDIO_BYTE_READER_MAIN__
+#endif // __TEST_BYTE_READERS_MAIN__
