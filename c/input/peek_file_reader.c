04d0581c863b078f7a4719640eb3f4319e2bfabe
John Kerl <kerl.john.r@gmail.com>
2015-09-07 21:34:32 -0400

    [read-performance iterate] ring-buffer iterate

--- c/input/peek_file_reader.c
+++ c/input/peek_file_reader.c
@@ -7,12 +7,22 @@ void pfr_dump(peek_file_reader_t* pfr) {
 	printf("======================== pfr at %p\n", pfr);
 	printf("  peekbuflen = %d\n", pfr->peekbuflen);
 	printf("  npeeked    = %d\n", pfr->npeeked);
+	int sob = pfr->sob;
+	int eob = (pfr->sob + pfr->npeeked) & pfr->peekbuflenmask;
 	for (int i = 0; i < pfr->peekbuflen; i++) {
 		char c = pfr->peekbuf[i];
-		char* sdesc = (i == pfr->sob) ? "START" : "";
-		char* edesc = (i == pfr->sob + pfr->npeeked - 1) ? "END" : "";
-		printf("  %-5s %-5s i=%2d c=%c [%02x]\n",
-			sdesc, edesc, i, isprint((unsigned char)c) ? c : ' ', c);
+		char* sdesc = (i == sob) ? "START" : "";
+		char* edesc = (i == eob) ? "END" : "";
+		char* occdesc = "";
+		if (sob <= eob) {
+			if (sob <= i && i < eob)
+				occdesc = "OCC";
+		} else {
+			if (i < sob || eob <= i)
+				occdesc = "OCC";
+		}
+		printf("  %-5s %-5s %-3s i=%2d c=%c [%02x]\n",
+			sdesc, edesc, occdesc, i, isprint((unsigned char)c) ? c : ' ', c);
 	}
 	printf("------------------------\n");
 }
