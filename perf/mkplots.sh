df8674778e262032b90dabc29405c95cdc168de2
John Kerl <kerl.john.r@gmail.com>
2015-09-20 10:03:30 -0400

    complete pull request; to-do

--- perf/mkplots.sh
+++ perf/mkplots.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-experiments=$(mlr cut -f experiment < tn.out|sort -u|sed 's/experiment=//')
+experiments=`mlr cut -f experiment < tn.out|sort -u|sed 's/experiment=//'`
 for experiment in $experiments; do
   mlr --onidx --ofs ' ' filter '$experiment=="'$experiment'"' then cut -x -f experiment tn.out | pgr -nc -title $experiment -xlabel nlines -ylabel seconds -legend 'a b' -lop &
 done
