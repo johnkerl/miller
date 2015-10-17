18820a3517e8030637d69ff461e318297f8fc616
John Kerl <kerl.john.r@gmail.com>
2015-10-17 18:29:02 -0400

    Miller 2.3.0

--- c/mlrvers.h
+++ c/mlrvers.h
@@ -1,5 +1,5 @@
 #ifndef MLRVERS_H
 #define MLRVERS_H
 // Manually increment on updates to https://github.com/johnkerl/miller/releases
-#define MLR_VERSION "v2.2.1"
+#define MLR_VERSION "v2.3.0"
 #endif // MLRVERS_H
