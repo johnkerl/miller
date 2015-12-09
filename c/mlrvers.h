00724f2ba53c6ab129544551a0ca5333b967ca23
John Kerl <kerl.john.r@gmail.com>
2015-12-09 09:35:22 -0500

    csv-read performance iterate

--- c/mlrvers.h
+++ c/mlrvers.h
@@ -1,5 +1,5 @@
 #ifndef MLRVERS_H
 #define MLRVERS_H
 // Manually increment on updates to https://github.com/johnkerl/miller/releases
-#define MLR_VERSION "v3.1.2"
+#define MLR_VERSION "v3.1.2-dev"
 #endif // MLRVERS_H
