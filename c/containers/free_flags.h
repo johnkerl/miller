fbba5d258fb0c1ddcbab3dc1fb9785555932fc97
John Kerl <kerl.john.r@gmail.com>
2015-12-09 09:57:11 -0500

    csv-read performance iterate

---
+++ c/containers/free_flags.h
@@ -0,0 +1,13 @@
+#ifndef FREE_FLAGS_H
+#define FREE_FLAGS_H
+
+// For use by various data structures including slls_t and lrec_t.  Some
+// keys/values are dynamically allocated and should be freed the container's
+// destructor, and some should not. Examples of the former include strduped
+// keys/values; examples of the latter include data from string literals, or
+// from mmapped file-input data.
+
+#define FREE_ENTRY_KEY   0x80
+#define FREE_ENTRY_VALUE 0x08
+
+#endif // FREE_FLAGS_H
