615e6ce631a4e767104b1609a40dc6a957567682
John Kerl <kerl.john.r@gmail.com>
2015-12-26 12:48:24 -0500

    valgrind findings

--- c/input/lrec_reader_stdio_csv.c
+++ c/input/lrec_reader_stdio_csv.c
@@ -157,6 +157,9 @@ static void lrec_reader_stdio_csv_free(lrec_reader_t* preader) {
 	parse_trie_free(pstate->pdquote_parse_trie);
 	rslls_free(pstate->pfields);
 	sb_free(pstate->psb);
+	free(pstate->ifs_eof);
+	free(pstate->dquote_irs);
+	free(pstate->dquote_ifs);
 	free(pstate);
 	free(preader);
 }
