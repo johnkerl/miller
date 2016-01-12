9829b9db02b3a171a3a1b88430a8fcca2c02775c
John Kerl <kerl.john.r@gmail.com>
2016-01-11 21:26:32 -0500

    filter-put merge iterate

--- c/dsls/filter_dsl_wrapper.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILTER_DSL_WRAPPER_H
-#define FILTER_DSL_WRAPPER_H
-#include  "../containers/mlr_dsl_ast.h"
-
-mlr_dsl_ast_node_holder_t* filter_dsl_parse(char* string);
-
-#endif // FILTER_DSL_WRAPPER_H
