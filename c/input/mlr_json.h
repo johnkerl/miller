d5e38cb90c6b9c51c4d523eef8e8e12eb71c5ddc
John Kerl <kerl.john.r@gmail.com>
2016-02-01 09:16:00 -0500

    json-input iterate

--- c/input/mlr_json.h
+++ c/input/mlr_json.h
@@ -2,4 +2,38 @@
 
 #ifndef MLR_JSON_H
 #define MLR_JSON_H
+
+#include "input/json.h"
+#include "containers/lrec.h"
+
+
+// ----------------------------------------------------------------
+// xxx transfer func:
+// input: top-level json value
+// input: current sllv of object
+// output: appended sllv
+// json value will be freed, or transferred to the sllv
+
+void transfer_objects(json_value_t* ptop_level_json, sllv_t* pobjects);
+
+		// xxx
+		//switch(parsed_top_level_json->type) {
+		//case json_array:
+		//	for each {
+		//		validate & add it
+		//	}
+		//	break;
+		//case json_object:
+		//	validate & add it
+		//	break;
+		//default:
+		//	break;
+		//}
+
+// ----------------------------------------------------------------
+// xxx validate func: return object or die
+
+json_value_t* validate_millerable_object(json_value_t* pjson);
+
+
 #endif // MLR_JSON_H
