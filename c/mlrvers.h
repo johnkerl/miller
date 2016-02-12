bddd41b7faf15ce3c18d6fe91a3fed32fea388e0
John Kerl <kerl.john.r@gmail.com>
2016-02-11 19:32:38 -0500

    JSON-input string-preserve iterate

--- c/mlrvers.h
+++ c/mlrvers.h
@@ -1,5 +1,5 @@
 #ifndef MLRVERS_H
 #define MLRVERS_H
 // Manually increment on updates to https://github.com/johnkerl/miller/releases
-#define MLR_VERSION "v3.3.2-dev"
+#define MLR_VERSION "v4.0.0-almost"
 #endif // MLRVERS_H
