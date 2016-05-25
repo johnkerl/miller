ecfb7ada92d57ca6d6d15ca775fa3aa613c254a9
John Kerl <kerl.john.r@gmail.com>
2016-05-25 07:30:13 -0400

    basename argv0 for travis

--- c/lib/mlr_globals.c
+++ c/lib/mlr_globals.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
+#include <libgen.h>
 #include "lib/mlr_globals.h"
 
 mlr_globals_t MLR_GLOBALS = { .argv0 = "mlr", .ofmt = NULL, .popts = NULL };
 void mlr_global_init(char* argv0, char* ofmt, cli_opts_t* popts) {
-	MLR_GLOBALS.argv0  = argv0;
+	MLR_GLOBALS.argv0  = basename(argv0);
 	MLR_GLOBALS.ofmt   = ofmt;
 	MLR_GLOBALS.popts  = popts;
 }
