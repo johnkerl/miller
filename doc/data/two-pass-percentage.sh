39688e8c1bf3a3583f4292bf3ee1066831854db7
John Kerl <kerl.john.r@gmail.com>
2016-06-13 23:35:00 -0400

    two-pass cookbook example

---
+++ doc/data/two-pass-percentage.sh
@@ -0,0 +1,16 @@
+mlr --from data/small put -q '
+  # These are executed once per record, which is the first pass.
+  # The key is to use NR to index an out-of-stream variable to
+  # retain all the x-field values.
+  @x_min = min($x, @x_min);
+  @x_max = max($x, @x_max);
+  @x[NR] = $x;
+
+  # The second pass is in a for-loop in an end-block.
+  end {
+    for (nr,v in @x) {
+      @x_pct[nr] = 100 * (@x[nr] - @x_min) / (@x_max - @x_min);
+    }
+    emit @x_pct, "NR"
+  }
+'
