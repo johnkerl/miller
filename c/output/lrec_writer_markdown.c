c1f3c36a0b1f3ffd906310c123b8714ec2851e51
John Kerl <kerl.john.r@gmail.com>
2016-06-29 20:49:48 -0400

    --omd iterate

---
+++ c/output/lrec_writer_markdown.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include "containers/mixutil.h"
+#include "lib/mlrutil.h"
+#include "output/lrec_writers.h"
+
+typedef struct _lrec_writer_markdown_state_t {
+	int   onr;
+	char* ors;
+	long long num_header_lines_output;
+	slls_t* plast_header_output;
+} lrec_writer_markdown_state_t;
+
+static void lrec_writer_markdown_free(lrec_writer_t* pwriter);
+static void lrec_writer_markdown_process(FILE* output_stream, lrec_t* prec, void* pvstate);
+
+// ----------------------------------------------------------------
+lrec_writer_t* lrec_writer_markdown_alloc(char* ors) {
+	lrec_writer_t* plrec_writer = mlr_malloc_or_die(sizeof(lrec_writer_t));
+
+	lrec_writer_markdown_state_t* pstate = mlr_malloc_or_die(sizeof(lrec_writer_markdown_state_t));
+	pstate->onr                     = 0;
+	pstate->ors                     = ors;
+	pstate->num_header_lines_output = 0LL;
+	pstate->plast_header_output     = NULL;
+
+	plrec_writer->pvstate       = (void*)pstate;
+	plrec_writer->pprocess_func = lrec_writer_markdown_process;
+	plrec_writer->pfree_func    = lrec_writer_markdown_free;
+
+	return plrec_writer;
+}
+
+static void lrec_writer_markdown_free(lrec_writer_t* pwriter) {
+	lrec_writer_markdown_state_t* pstate = pwriter->pvstate;
+	slls_free(pstate->plast_header_output);
+	free(pstate);
+	free(pwriter);
+}
+
+// ----------------------------------------------------------------
+static void lrec_writer_markdown_process(FILE* output_stream, lrec_t* prec, void* pvstate) {
+	if (prec == NULL)
+		return;
+	lrec_writer_markdown_state_t* pstate = pvstate;
+	char* ors = pstate->ors;
+
+	if (pstate->plast_header_output != NULL) {
+		if (!lrec_keys_equal_list(prec, pstate->plast_header_output)) {
+			slls_free(pstate->plast_header_output);
+			pstate->plast_header_output = NULL;
+			if (pstate->num_header_lines_output > 0LL)
+				fputs(ors, output_stream);
+		}
+	}
+
+	if (pstate->plast_header_output == NULL) {
+		int nf = 0;
+
+		fputs("| ", output_stream);
+		for (lrece_t* pe = prec->phead; pe != NULL; pe = pe->pnext) {
+			fputs(pe->key, output_stream);
+			fputs(" |", output_stream);
+			nf++;
+		}
+		fputs(ors, output_stream);
+
+		fputs("| ", output_stream);
+		for (lrece_t* pe = prec->phead; pe != NULL; pe = pe->pnext) {
+			fputs("---", output_stream);
+			fputs(" |", output_stream);
+			nf++;
+		}
+		fputs(ors, output_stream);
+
+		pstate->plast_header_output = mlr_copy_keys_from_record(prec);
+		pstate->num_header_lines_output++;
+	}
+
+	int nf = 0;
+	fputs("| ", output_stream);
+	for (lrece_t* pe = prec->phead; pe != NULL; pe = pe->pnext) {
+		fputs(pe->value, output_stream);
+		fputs(" |", output_stream);
+		nf++;
+	}
+	fputs(ors, output_stream);
+	pstate->onr++;
+
+	lrec_free(prec); // end of baton-pass
+}
