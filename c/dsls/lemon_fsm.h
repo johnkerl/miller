34eb6add2948c821ce8c0567e384bba6ef6079cd
John Kerl <kerl.john.r@gmail.com>
2016-07-14 09:42:23 -0400

    lemon refactor iterate

---
+++ c/dsls/lemon_fsm.h
@@ -0,0 +1,14 @@
+#ifndef LEMON_FSM_H
+#define LEMON_FSM_H
+
+#include "lemon_structs.h"
+
+// xxx protoize
+void FindRulePrecedences();
+void FindFirstSets();
+void FindStates();
+void FindLinks();
+void FindFollowSets();
+void FindActions();
+
+#endif // LEMON_FSM_H
