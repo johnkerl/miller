017c9eebd2b7bbb589566a068adb2099a5447dd8
John Kerl <kerl.john.r@gmail.com>
2016-07-20 19:15:06 -0400

    multi_lrec_writer for emit-write

---
+++ c/output/multi_lrec_writer.c
@@ -0,0 +1,103 @@
+#include "lib/mlrutil.h"
+#include "lib/mlr_globals.h"
+#include "cli/mlrcli.h"
+#include "output/multi_lrec_writer.h"
+
+// ----------------------------------------------------------------
+multi_lrec_writer_t* multi_lrec_writer_alloc() {
+	multi_lrec_writer_t* pmlw = mlr_malloc_or_die(sizeof(multi_lrec_writer_t));
+	pmlw->pnames_to_lrec_writers_and_fps = lhmsv_alloc();
+	return pmlw;
+}
+
+// ----------------------------------------------------------------
+void multi_lrec_writer_free(multi_lrec_writer_t* pmlw) {
+	if (pmlw == NULL)
+		return;
+
+	lhmsv_free(pmlw->pnames_to_lrec_writers_and_fps);
+	free(pmlw);
+}
+
+//// ----------------------------------------------------------------
+//static inline FILE* multi_lrec_writer_get(multi_lrec_writer_t* pmlw, char* filename,
+//	char* mode, char* mode_desc)
+//{
+//	FILE* outfp = lhmsv_get(pmlw->pnames_to_fps, filename);
+//	if (outfp == NULL) {
+//		outfp = fopen(filename, mode);
+//		if (outfp == NULL) {
+//			perror("fopen");
+//			fprintf(stderr, "%s: failed fopen for %s of \"%s\".\n",
+//				MLR_GLOBALS.bargv0, mode_desc, filename);
+//			exit(1);
+//		}
+//		lhmsv_put(pmlw->pnames_to_fps, mlr_strdup_or_die(filename), outfp, FREE_ENTRY_KEY);
+//	}
+//	return outfp;
+//}
+//
+//// ----------------------------------------------------------------
+//FILE* multi_lrec_writer_get_for_write(multi_lrec_writer_t* pmlw, char* filename) {
+//	return multi_lrec_writer_get(pmlw, filename, "w", "write");
+//}
+//
+//// ----------------------------------------------------------------
+//FILE* multi_lrec_writer_get_for_append(multi_lrec_writer_t* pmlw, char* filename) {
+//	return multi_lrec_writer_get(pmlw, filename, "a", "append");
+//}
+
+static void common_handler(multi_lrec_writer_t* pmlw, sllv_t* poutrecs, char* filename, int flush_every_record,
+	char* mode, char* mode_desc) {
+	if (poutrecs == NULL) // synonym for empty record-list
+		return;
+	lrec_writer_and_fp_t* pstate = lhmsv_get(pmlw->pnames_to_lrec_writers_and_fps, filename);
+	if (pstate == NULL) {
+		pstate = mlr_malloc_or_die(sizeof(lrec_writer_and_fp_t)); // xxx free at destruct
+		cli_opts_t* popts = MLR_GLOBALS.popts;
+		pstate->plrec_writer = lrec_writer_alloc(popts->ofile_fmt, popts->ors, popts->ofs, popts->ops,
+			popts->headerless_csv_output, popts->oquoting, FALSE /* xxx left_align_pprint*/,
+			popts->right_justify_xtab_value, popts->json_flatten_separator, popts->quote_json_values_always,
+			popts->stack_json_output_vertically, popts->wrap_json_output_in_outer_list);
+		if (pstate->plrec_writer == NULL) {
+			fprintf(stderr, "%s: internal coding error detected in file \"%s\" at line %d.\n",
+				MLR_GLOBALS.bargv0, __FILE__, __LINE__);
+			exit(1);
+		}
+		pstate->output_stream = fopen(filename, mode);
+		if (pstate->output_stream == NULL) {
+			perror("fopen");
+			fprintf(stderr, "%s: fopen error on \"%s\".\n", MLR_GLOBALS.bargv0, filename);
+			exit(1);
+		}
+
+		lhmsv_put(pmlw->pnames_to_lrec_writers_and_fps, mlr_strdup_or_die(filename), pstate, FREE_ENTRY_KEY);
+	}
+
+	// xxx use destructive pop-off logic for the sllv
+	while (poutrecs->phead) {
+		lrec_t* poutrec = sllv_pop(poutrecs);
+
+		pstate->plrec_writer->pprocess_func(pstate->output_stream, poutrec, pstate->plrec_writer->pvstate);
+
+		if (poutrec != NULL) {
+			if (flush_every_record)
+				fflush(pstate->output_stream);
+		} else {
+			if (fclose(pstate->output_stream) != 0) {
+				perror("fclose");
+				fprintf(stderr, "%s: fclose error on \"%s\".\n", MLR_GLOBALS.bargv0, filename);
+				exit(1);
+			}
+			pstate->output_stream = NULL;
+		}
+	}
+}
+
+void multi_lrec_writer_write(multi_lrec_writer_t* pmlw, sllv_t* poutrecs, char* filename, int flush_every_record) {
+	common_handler(pmlw, poutrecs, filename, flush_every_record, "w", "write");
+}
+
+void multi_lrec_writer_append(multi_lrec_writer_t* pmlw, sllv_t* poutrecs, char* filename, int flush_every_record) {
+	common_handler(pmlw, poutrecs, filename, flush_every_record, "a", "append");
+}
---
+++ c/output/multi_lrec_writer.h
@@ -0,0 +1,27 @@
+#ifndef MULTI_LREC_WRITER_H
+#define MULTI_LREC_WRITER_H
+
+#include <stdio.h>
+#include "containers/lhmsv.h"
+#include "containers/sllv.h"
+#include "output/lrec_writers.h"
+
+// ----------------------------------------------------------------
+typedef struct _lrec_writer_and_fp_t {
+	lrec_writer_t* plrec_writer;
+	FILE* output_stream;
+} lrec_writer_and_fp_t;
+
+typedef struct _multi_lrec_writer_t {
+	// xxx to do: bound the number of open files and LRU them.
+	lhmsv_t* pnames_to_lrec_writers_and_fps;
+} multi_lrec_writer_t;
+
+// ----------------------------------------------------------------
+multi_lrec_writer_t* multi_lrec_writer_alloc();
+void multi_lrec_writer_free(multi_lrec_writer_t* pmlw);
+
+void multi_lrec_writer_write(multi_lrec_writer_t* pmlw, sllv_t* poutrecs, char* filename, int flush_every_record);
+void multi_lrec_writer_append(multi_lrec_writer_t* pmlw, sllv_t* poutrecs, char* filename, int flush_every_record);
+
+#endif // MULTI_LREC_WRITER_H
