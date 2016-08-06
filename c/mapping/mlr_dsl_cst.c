ca199593ee4c5c84c0dd8a2e46de22253eac4897
John Kerl <kerl.john.r@gmail.com>
2016-08-06 11:58:31 -0400

    doc neaten

--- c/mapping/mlr_dsl_cst.c
+++ c/mapping/mlr_dsl_cst.c
@@ -2592,25 +2592,25 @@ static void mlr_dsl_filter_keyword_usage(FILE* ostream) {
     fprintf(ostream,
 		"filter: includes/excludes the record in the output record stream.\n"
 		"\n"
-		"  Example: mlr put 'filter (NR == 2 || $x > 5.4)'.\n"
+		"  Example: mlr --from myfile.dat put 'filter (NR == 2 || $x > 5.4)'\n"
 		"\n"
 		"  Instead of put with 'filter false' you can simply use put -q.  The following\n"
 		"  uses the input record to accumulate data but only prints the running sum\n"
 		"  without printing the input record:\n"
 		"\n"
-		"  Example: mlr put -q '@running_sum += $x * $y; emit @running_sum'.\n");
+		"  Example: mlr --from myfile.dat put -q '@running_sum += $x * $y; emit @running_sum'\n");
 }
 
 static void mlr_dsl_unset_keyword_usage(FILE* ostream) {
     fprintf(ostream,
 		"unset: clears field(s) from the current record, or an out-of-stream variable.\n"
 		"\n"
-		"  Example: mlr put 'unset $x'\n"
-		"  Example: mlr put 'unset $*'\n"
-		"  Example: mlr put 'for (k, v in $*) { if (k =~ \"a.*\") { unset $[k] } }'\n"
-		"  Example: mlr put '...; unset @sums'\n"
-		"  Example: mlr put '...; unset @sums[\"green\"]'\n"
-		"  Example: mlr put '...; unset @*'\n");
+		"  Example: mlr --from myfile.dat put 'unset $x'\n"
+		"  Example: mlr --from myfile.dat put 'unset $*'\n"
+		"  Example: mlr --from myfile.dat put 'for (k, v in $*) { if (k =~ \"a.*\") { unset $[k] } }'\n"
+		"  Example: mlr --from myfile.dat put '...; unset @sums'\n"
+		"  Example: mlr --from myfile.dat put '...; unset @sums[\"green\"]'\n"
+		"  Example: mlr --from myfile.dat put '...; unset @*'\n");
 }
 
 static void mlr_dsl_tee_keyword_usage(FILE* ostream) {
@@ -2623,10 +2623,11 @@ static void mlr_dsl_tee_keyword_usage(FILE* ostream) {
 		"  process which will process the data. There will be one subordinate process for\n"
 		"  each distinct value of the piped-to command.\n"
 		"\n"
-		"  Example: mlr put 'tee >  \"/tmp/data-\".$a, $*'\n"
-		"  Example: mlr put 'tee >> \"/tmp/data-\".$a.$b, $*'\n"
-		"  Example: mlr put -q 'tee | \"tr \[a-z\\] \[A-Z\\]\", $*'\n"
-		"  Example: mlr put -q 'tee | \"tr \[a-z\\] \[A-Z\\] > /tmp/data-\".$a, $*'\n");
+		"  Example: mlr --from myfile.dat put 'tee >  \"/tmp/data-\".$a, $*'\n"
+		"  Example: mlr --from myfile.dat put 'tee >> \"/tmp/data-\".$a.$b, $*'\n"
+		"  Example: mlr --from myfile.dat put 'tee >  stderr, $*'\n"
+		"  Example: mlr --from myfile.dat put -q 'tee | \"tr \[a-z\\] \[A-Z\\]\", $*'\n"
+		"  Example: mlr --from myfile.dat put -q 'tee | \"tr \[a-z\\] \[A-Z\\] > /tmp/data-\".$a, $*'\n");
 }
 
 static void mlr_dsl_emit_keyword_usage(FILE* ostream) {
@@ -2640,12 +2641,13 @@ static void mlr_dsl_emit_keyword_usage(FILE* ostream) {
 		"  record. The | is for pipe to a process which will process the data. There will\n"
 		"  be one subordinate process for each distinct value of the piped-to command.\n"
 		"\n"
-		"  Example: mlr put '... ; emit @sums'\n"
-		"  Example: mlr put '... ; emit @sums, \"index1\", \"index2\"'\n"
-		"  Example: mlr put '... ; emit @*, \"index1\", \"index2\"'\n"
-		"  Example: mlr put '... ; emit >  \"mytap.dat\", @*, \"index1\", \"index2\"'\n"
-		"  Example: mlr put '... ; emit >> \"mytap.dat\", @*, \"index1\", \"index2\"'\n"
-		"  Example: mlr put '... ; emit | \"grep somepattern\", @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emit @sums'\n"
+		"  Example: mlr --from myfile.dat put '... ; emit @sums, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emit @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emit >  \"mytap.dat\", @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emit >> \"mytap.dat\", @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emit > stderr, @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emit | \"grep somepattern\", @*, \"index1\", \"index2\"'\n"
 		"\n"
 		"  Please see http://johnkerl.org/miller/doc for more information.\n");
 }
@@ -2662,12 +2664,13 @@ static void mlr_dsl_emitp_keyword_usage(FILE* ostream) {
 		"  record. The | is for pipe to a process which will process the data. There will\n"
 		"  be one subordinate process for each distinct value of the piped-to command.\n"
 		"\n"
-		"  Example: mlr put '... ; emitp @sums'\n"
-		"  Example: mlr put '... ; emitp @sums, \"index1\", \"index2\"'\n"
-		"  Example: mlr put '... ; emitp @*, \"index1\", \"index2\"'\n"
-		"  Example: mlr put '... ; emitp >  \"mytap.dat\", @*, \"index1\", \"index2\"'\n"
-		"  Example: mlr put '... ; emitp >> \"mytap.dat\", @*, \"index1\", \"index2\"'\n"
-		"  Example: mlr put '... ; emitp | \"grep somepattern\", @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitp @sums'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitp @sums, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitp @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitp >  \"mytap.dat\", @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitp >> \"mytap.dat\", @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitp > stderr, @*, \"index1\", \"index2\"'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitp | \"grep somepattern\", @*, \"index1\", \"index2\"'\n"
 		"\n"
 		"  Please see http://johnkerl.org/miller/doc for more information.\n");
 }
@@ -2683,12 +2686,13 @@ static void mlr_dsl_emitf_keyword_usage(FILE* ostream) {
 		"  record. The | is for pipe to a process which will process the data. There will\n"
 		"  be one subordinate process for each distinct value of the piped-to command.\n"
 		"\n"
-		"  Example: mlr put '... ; emitf @a'\n"
-		"  Example: mlr put '... ; emitf @a, @b, @c'\n"
-		"  Example: mlr put '... ; emitf > \"mytap.dat\", @a, @b, @c'\n"
-		"  Example: mlr put '... ; emitf >> \"mytap.dat\", @a, @b, @c'\n"
-		"  Example: mlr put '... ; emitf | \"grep somepattern\", @a, @b, @c'\n"
-		"  Example: mlr put '... ; emitf | \"grep somepattern > mytap.dat\", @a, @b, @c'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitf @a'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitf @a, @b, @c'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitf > \"mytap.dat\", @a, @b, @c'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitf >> \"mytap.dat\", @a, @b, @c'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitf > stderr, @a, @b, @c'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitf | \"grep somepattern\", @a, @b, @c'\n"
+		"  Example: mlr --from myfile.dat put '... ; emitf | \"grep somepattern > mytap.dat\", @a, @b, @c'\n"
 		"\n"
 		"  Please see http://johnkerl.org/miller/doc for more information.\n");
 }
@@ -2696,49 +2700,64 @@ static void mlr_dsl_emitf_keyword_usage(FILE* ostream) {
 static void mlr_dsl_dump_keyword_usage(FILE* ostream) {
     fprintf(ostream,
 		"dump: prints all currently defined out-of-stream variables immediately\n"
-		"  to stdout as JSON.\n");
+		"  to stdout as JSON.\n"
+		"\n"
+		"  With >, >>, or |, the data do not become part of the output record stream but\n"
+		"  are instead redirected.  The > and >> are for write and append, as in the\n"
+		"  shell, but (as with awk) the file-overwrite for > is on first write, not per\n"
+		"  record. The | is for pipe to a process which will process the data. There will\n"
+		"  be one subordinate process for each distinct value of the piped-to command.\n"
+		"\n"
+		"  Example: mlr --from myfile.dat put -q '@v[NR]=$*; end { dump }'\n"
+		"  Example: mlr --from myfile.dat put -q '@v[NR]=$*; end { dump >  \"mytap.dat\"}'\n"
+		"  Example: mlr --from myfile.dat put -q '@v[NR]=$*; end { dump >> \"mytap.dat\"}'\n"
+		"  Example: mlr --from myfile.dat put -q '@v[NR]=$*; end { dump | \"jq .[]\"}'\n");
 }
 
 static void mlr_dsl_edump_keyword_usage(FILE* ostream) {
     fprintf(ostream,
 		"edump: prints all currently defined out-of-stream variables immediately\n"
-		"  to stderr as JSON.\n");
+		"  to stderr as JSON.\n"
+		"\n"
+		"  Example: mlr --from myfile.dat put -q '@v[NR]=$*; end { edump }'\n");
 }
 
 static void mlr_dsl_print_keyword_usage(FILE* ostream) {
     fprintf(ostream,
 		"print: prints expression immediately to stdout.\n"
-		"  Example: mlr put -q 'print \"The sum of x and y is \".($x+$y)'.\n"
-		"  Example: mlr put -q 'for (k, v in $*) { print k . \" => \" . v }'.\n");
+		"  Example: mlr --from myfile.dat put -q 'print \"The sum of x and y is \".($x+$y)'\n"
+		"  Example: mlr --from myfile.dat put -q 'for (k, v in $*) { print k . \" => \" . v }'\n"
+		"  Example: mlr --from myfile.dat put  '(NR %% 1000 == 0) { print > stderr, \"Checkpoint \".NR}'\n");
 }
 
 static void mlr_dsl_printn_keyword_usage(FILE* ostream) {
     fprintf(ostream,
 		"printn: prints expression immediately to stdout, without trailing newline.\n"
-		"  Example: mlr put -q 'printn \"The sum of x and y is \".($x+$y); print \"\"'.\n");
+		"  Example: mlr --from myfile.dat put -q 'printn \".\"; end { print \"\" }'\n");
 }
 
 static void mlr_dsl_eprint_keyword_usage(FILE* ostream) {
     fprintf(ostream,
 		"eprint: prints expression immediately to stderr.\n"
-		"  Example: mlr put -q 'eprint \"The sum of x and y is \".($x+$y)'.\n"
-		"  Example: mlr put -q 'for (k, v in $*) { eprint k . \" => \" . v }'.\n");
+		"  Example: mlr --from myfile.dat put -q 'eprint \"The sum of x and y is \".($x+$y)'\n"
+		"  Example: mlr --from myfile.dat put -q 'for (k, v in $*) { eprint k . \" => \" . v }'\n"
+		"  Example: mlr --from myfile.dat put  '(NR %% 1000 == 0) { eprint \"Checkpoint \".NR}'\n");
 }
 
 static void mlr_dsl_eprintn_keyword_usage(FILE* ostream) {
     fprintf(ostream,
 		"eprintn: prints expression immediately to stderr, without trailing newline.\n"
-		"  Example: mlr put -q 'eprintn \"The sum of x and y is \".($x+$y)'; eprint \"\".\n");
+		"  Example: mlr --from myfile.dat put -q 'eprintn \"The sum of x and y is \".($x+$y)'; eprint \"\"\n");
 }
 
 static void mlr_dsl_stdout_keyword_usage(FILE* ostream) {
     fprintf(ostream,
-		"stdout: Used for tee, emit, emitf, emitp, and dump in place of filename to\n"
-		"  print to standard output.\n");
+		"stdout: Used for tee, emit, emitf, emitp, print, and dump in place of filename\n"
+		"  to print to standard output.\n");
 }
 
 static void mlr_dsl_stderr_keyword_usage(FILE* ostream) {
     fprintf(ostream,
-		"stderr: Used for tee, emit, emitf, emitp, and dump in place of filename to\n"
-		"  print to standard error.\n");
+		"stderr: Used for tee, emit, emitf, emitp, print, and dump in place of filename\n"
+		"  to print to standard error.\n");
 }
