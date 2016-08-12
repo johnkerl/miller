805897845815687da18cae71e6d50849a3382c08
John Kerl <kerl.john.r@gmail.com>
2016-08-11 23:53:15 -0400

    post-4.4.0

--- c/mlrvers.h
+++ c/mlrvers.h
@@ -1,5 +1,5 @@
 #ifndef MLRVERS_H
 #define MLRVERS_H
 // Manually increment on updates to https://github.com/johnkerl/miller/releases
-#define MLR_VERSION "v4.4.0"
+#define MLR_VERSION "v4.4.0-dev"
 #endif // MLRVERS_H
