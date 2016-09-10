360a57c69dcd039b2bf8ceb5f2ba6ec86077c952
John Kerl <kerl.john.r@gmail.com>
2016-09-09 21:16:57 -0400

    cst.c split-up

--- c/mapping/mlr_dsl_cst.c
+++ c/mapping/mlr_dsl_cst.c
@@ -1711,6 +1711,76 @@ static mlr_dsl_cst_statement_t* alloc_emit_lashed(mlr_dsl_ast_node_t* pnode, fmg
 }
 
 // ----------------------------------------------------------------
+// Example ASTs, with and without indexing on the left-hand-side oosvar name:
+
+// $ mlr -n put -v '@x[1]["2"][$3][@4]=5'
+// AST ROOT:
+// text="list", type=statement_list:
+//     text="=", type=oosvar_assignment:
+//         text="oosvar_keylist", type=oosvar_keylist:
+//             text="x", type=string_literal.
+//             text="1", type=strnum_literal.
+//             text="2", type=strnum_literal.
+//             text="3", type=field_name.
+//             text="oosvar_keylist", type=oosvar_keylist:
+//                 text="4", type=string_literal.
+//         text="5", type=strnum_literal.
+//
+// $ mlr -n put -v '@x = $y'
+// AST ROOT:
+// text="list", type=statement_list:
+//     text="=", type=oosvar_assignment:
+//         text="oosvar_keylist", type=oosvar_keylist:
+//             text="x", type=string_literal.
+//         text="y", type=field_name.
+//
+// $ mlr -n put -q -v 'emit @v, "a", "b", "c"'
+// AST ROOT:
+// text="list", type=statement_list:
+//     text="emit", type=emit:
+//         text="emit", type=emit:
+//             text="oosvar_keylist", type=oosvar_keylist:
+//                 text="v", type=string_literal.
+//             text="emit_namelist", type=emit:
+//                 text="a", type=strnum_literal.
+//                 text="b", type=strnum_literal.
+//                 text="c", type=strnum_literal.
+//         text="stream", type=stream:
+//
+// $ mlr -n put -q -v 'emit @v[1][2], "a", "b","c"'
+// AST ROOT:
+// text="list", type=statement_list:
+//     text="emit", type=emit:
+//         text="emit", type=emit:
+//             text="oosvar_keylist", type=oosvar_keylist:
+//                 text="v", type=string_literal.
+//                 text="1", type=strnum_literal.
+//                 text="2", type=strnum_literal.
+//             text="emit_namelist", type=emit:
+//                 text="a", type=strnum_literal.
+//                 text="b", type=strnum_literal.
+//                 text="c", type=strnum_literal.
+//         text="stream", type=stream:
+
+// pnode is input; pkeylist_evaluators is appended to.
+static sllv_t* allocate_keylist_evaluators_from_oosvar_node(mlr_dsl_ast_node_t* pnode, fmgr_t* pfmgr,
+	int type_inferencing, int context_flags)
+{
+	sllv_t* pkeylist_evaluators = sllv_alloc();
+
+	for (sllve_t* pe = pnode->pchildren->phead; pe != NULL; pe = pe->pnext) {
+		mlr_dsl_ast_node_t* pkeynode = pe->pvvalue;
+		if (pkeynode->type == MD_AST_NODE_TYPE_STRING_LITERAL) {
+			sllv_append(pkeylist_evaluators, rval_evaluator_alloc_from_string(pkeynode->text));
+		} else {
+			sllv_append(pkeylist_evaluators, rval_evaluator_alloc_from_ast(pkeynode, pfmgr,
+				type_inferencing, context_flags));
+		}
+	}
+	return pkeylist_evaluators;
+}
+
+// ----------------------------------------------------------------
 static mlr_dsl_cst_statement_t* alloc_dump(mlr_dsl_ast_node_t* pnode, fmgr_t* pfmgr, lhmsv_t* pcst_subroutine_states,
 	int type_inferencing, int context_flags)
 {
@@ -2962,74 +3032,4 @@ static void handle_print(
 	if (sfree_flags)
 		free(sval);
 	mv_free(&val);
-}
-
-// ----------------------------------------------------------------
-// Example ASTs, with and without indexing on the left-hand-side oosvar name:
-
-// $ mlr -n put -v '@x[1]["2"][$3][@4]=5'
-// AST ROOT:
-// text="list", type=statement_list:
-//     text="=", type=oosvar_assignment:
-//         text="oosvar_keylist", type=oosvar_keylist:
-//             text="x", type=string_literal.
-//             text="1", type=strnum_literal.
-//             text="2", type=strnum_literal.
-//             text="3", type=field_name.
-//             text="oosvar_keylist", type=oosvar_keylist:
-//                 text="4", type=string_literal.
-//         text="5", type=strnum_literal.
-//
-// $ mlr -n put -v '@x = $y'
-// AST ROOT:
-// text="list", type=statement_list:
-//     text="=", type=oosvar_assignment:
-//         text="oosvar_keylist", type=oosvar_keylist:
-//             text="x", type=string_literal.
-//         text="y", type=field_name.
-//
-// $ mlr -n put -q -v 'emit @v, "a", "b", "c"'
-// AST ROOT:
-// text="list", type=statement_list:
-//     text="emit", type=emit:
-//         text="emit", type=emit:
-//             text="oosvar_keylist", type=oosvar_keylist:
-//                 text="v", type=string_literal.
-//             text="emit_namelist", type=emit:
-//                 text="a", type=strnum_literal.
-//                 text="b", type=strnum_literal.
-//                 text="c", type=strnum_literal.
-//         text="stream", type=stream:
-//
-// $ mlr -n put -q -v 'emit @v[1][2], "a", "b","c"'
-// AST ROOT:
-// text="list", type=statement_list:
-//     text="emit", type=emit:
-//         text="emit", type=emit:
-//             text="oosvar_keylist", type=oosvar_keylist:
-//                 text="v", type=string_literal.
-//                 text="1", type=strnum_literal.
-//                 text="2", type=strnum_literal.
-//             text="emit_namelist", type=emit:
-//                 text="a", type=strnum_literal.
-//                 text="b", type=strnum_literal.
-//                 text="c", type=strnum_literal.
-//         text="stream", type=stream:
-
-// pnode is input; pkeylist_evaluators is appended to.
-static sllv_t* allocate_keylist_evaluators_from_oosvar_node(mlr_dsl_ast_node_t* pnode, fmgr_t* pfmgr,
-	int type_inferencing, int context_flags)
-{
-	sllv_t* pkeylist_evaluators = sllv_alloc();
-
-	for (sllve_t* pe = pnode->pchildren->phead; pe != NULL; pe = pe->pnext) {
-		mlr_dsl_ast_node_t* pkeynode = pe->pvvalue;
-		if (pkeynode->type == MD_AST_NODE_TYPE_STRING_LITERAL) {
-			sllv_append(pkeylist_evaluators, rval_evaluator_alloc_from_string(pkeynode->text));
-		} else {
-			sllv_append(pkeylist_evaluators, rval_evaluator_alloc_from_ast(pkeynode, pfmgr,
-				type_inferencing, context_flags));
-		}
-	}
-	return pkeylist_evaluators;
 }
