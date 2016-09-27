4ba45b0b2f1f7562d0f023b315eb6c16c95f6d23
John Kerl <kerl.john.r@gmail.com>
2016-09-27 17:08:43 -0400

    local-var doc iterate

--- doc/data/local-example-1.sh
+++ doc/data/local-example-1.sh
@@ -1,13 +1,13 @@
 mlr seqgen --start 1 --stop 10 then put '
-  func f(a, b) {
-      r = 0.0;
-      for (local i = 0; i < 6; i += 1) {
-          local u = urand();
-          r += u;
+  func f(a, b) {                          # function arguments a and b
+      r = 0.0;                            # local r scoped to the function
+      for (local i = 0; i < 6; i += 1) {  # local i scoped to the for-loop
+          local u = urand();              # local u scoped to the for-loop
+          r += u;                         # updates r from the enclosing scope
       }
       r /= 6;
       return a + (b - a) * r;
   }
-  local o = f(10, 20);
+  local o = f(10, 20);                    # local to the top-level scope
   $o = o;
 '
