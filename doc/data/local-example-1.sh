09cd0c894fb39bc29adeed4457d2a9198fcecba8
John Kerl <kerl.john.r@gmail.com>
2016-09-28 18:57:25 -0400

    neaten

--- doc/data/local-example-1.sh
+++ doc/data/local-example-1.sh
@@ -1,4 +1,7 @@
-mlr seqgen --start 1 --stop 10 then put '
+# Here I'm using a specified random-number seed so this example always
+# produces the same output for this web document: in everyday practice we
+# would leave off the --seed 12345 part.
+mlr --seed 12345 seqgen --start 1 --stop 10 then put '
   func f(a, b) {                          # function arguments a and b
       r = 0.0;                            # local r scoped to the function
       for (local i = 0; i < 6; i += 1) {  # local i scoped to the for-loop
