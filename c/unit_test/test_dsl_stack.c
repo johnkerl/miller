ae9264d66ead3ec418378fc17a78824da894c1a1
John Kerl <kerl.john.r@gmail.com>
2016-10-18 20:52:26 -0400

    test-dsl-stack UT

--- c/unit_test/test_dsl_stack.c
+++ c/unit_test/test_dsl_stack.c
@@ -13,6 +13,20 @@ int assertions_failed = 0;
 
 static char* sep = "================================================================";
 
+// ----------------------------------------------------------------
+// xxx being tested:
+// * placements
+// * clears
+// * stack-frame over/underflow
+// * correct bindings including ispresent
+
+// xxx:
+// ? begin top-level
+// ? main  top-level
+// ? end   top-level
+// ? func  top-level
+// ? subr  top-level
+
 // ================================================================
 // LOCAL SUPPORTING FUNCTIONS
 
@@ -41,6 +55,12 @@ static mlr_dsl_cst_t* my_mlr_dsl_cst_alloc(mlr_dsl_ast_t* past) {
 static mv_t* imv(long long intv) {
 	mv_t* pmv = mlr_malloc_or_die(sizeof(mv_t));
 	*pmv = mv_from_int(intv);
+	return pmv;
+}
+
+static mv_t* smv(char* strv) {
+	mv_t* pmv = mlr_malloc_or_die(sizeof(mv_t));
+	*pmv = mv_from_string(strv, NO_FREE);
 	return pmv;
 }
 
@@ -205,54 +225,113 @@ static char* test_top_level_clears() {
 }
 
 // ----------------------------------------------------------------
-// being tested:
-// * placements
-// * clears
-// * stack-frame over/underflow
-// * correct bindings including ispresent
+static char* test_placements() {
+	printf("%s\n", sep);
+	char* mlr_dsl_expression = ""
+		"                                                      # absent-null is at local 0\n"
+		"@v[NR] = NR*1000;\n"
+		"true {\n"
+		"    local val = nonesuch;                             # local 1\n"
+		"    while (1 == 1) {\n"
+		"        if (NR == 1) {\n"
+		"            for (k, v in $*) {                        # locals 2 and 3\n"
+		"                val = v;\n"
+		"            }\n"
+		"        } elif (NR == 2) {\n"
+		"            for (k in @v) {                           # local 2\n"
+		"            }\n"
+		"        } elif (NR == 3) {\n"
+		"            for (k, v in @v) {                        # locals 2 and 3\n"
+		"            }\n"
+		"        } elif (NR == 4) {\n"
+		"            local sum = 0;                            # local 2\n"
+		"            for (local i = 1; i <= 10; i += 1) {      # local 3\n"
+		"                for (local j = 1; j <= 10; j += 1) {  # local 4\n"
+		"                    sum += i*j;\n"
+		"                }\n"
+		"            }\n"
+		"            val = sum;\n"
+		"        }\n"
+		"        break;\n"
+		"    }\n"
+		"}\n"
+		;
 
-// xxx assert placements & remaining absents
-// xxx drive another lrec (w/ if:NR etc.)
-// xxx assert clears
+	mlr_dsl_ast_t* past         = my_mlr_dsl_parse(mlr_dsl_expression);
+	mlr_dsl_cst_t* pcst         = my_mlr_dsl_cst_alloc(past);
+	variables_t*   pvariables   = my_make_variables();
+	cst_outputs_t* pcst_outputs = my_make_cst_outputs();
+	lrec_put(pvariables->pinrec, "xyz", "ijkl", NO_FREE);
 
-// xxx x all control structures (w/ nesting)
-// xxx from begin/end/main/func/subr
-// xxx also regtestrun:repro w/ all such cases
+	printf("Expression:\n");
+	printf("%s\n", mlr_dsl_expression);
 
+	mu_assert_lf(pcst != NULL);
+	mu_assert_lf(pcst->pmain_block->pframe != NULL);
+	mu_assert_lf(pcst->pmain_block->max_var_depth == 5);
+	mv_t* pvars = pcst->pmain_block->pframe->pvars;
 
-// ? begin top-level
-// ? main  top-level
-// ? end   top-level
-// ? func  top-level
-// ? subr  top-level
+	// Before any records are read
+	mu_assert_lf(mv_is_absent(&pvars[0]));
+	mu_assert_lf(mv_is_absent(&pvars[1]));
+	mu_assert_lf(mv_is_absent(&pvars[2]));
+	mu_assert_lf(mv_is_absent(&pvars[3]));
+	mu_assert_lf(mv_is_absent(&pvars[4]));
 
-// xxx:
-// cond true
-// if true elif true else
-// while true w/ break
-// for-srec w/ non-empty lrec & break
-// for full-oosvar w/ non-empty oos & break
-// for full-oosvar-key-only w/ non-empty oos & break
-// for oosvar w/ non-empty oos & break
-// for oosvar-key-only w/ non-empty oos & break
-// triple-for w/ break
+	// Push record 1
+	pvariables->pctx->nr = 1;
+	mlr_dsl_cst_handle_top_level_statement_block(pcst->pmain_block, pvariables, pcst_outputs);
+	printf("\n");
+	for (int i = 1; i <= 4; i++) { printf("1:%d:%s\n", i, mv_alloc_format_val(&pvars[i])); }
+	mu_assert_lf(mv_is_absent(&pvars[0]));
+	mu_assert_lf(mv_equals_si(&pvars[1], smv("ijkl")));
+	mu_assert_lf(mv_equals_si(&pvars[2], smv("xyz")));
+	mu_assert_lf(mv_equals_si(&pvars[3], smv("ijkl")));
+	mu_assert_lf(mv_is_absent(&pvars[4]));
 
-// ? md_cond_block
-// ? md_while_block
-// ? md_for_loop_full_srec
-// ? md_for_loop_full_oosvar
-// ? md_for_loop_full_oosvar_key_only
-// ? md_for_loop_oosvar
-// ? md_for_loop_oosvar_key_only
-// ? md_triple_for
-// ? md_if_chain
+	// Push record 2
+	pvariables->pctx->nr = 2;
+	mlr_dsl_cst_handle_top_level_statement_block(pcst->pmain_block, pvariables, pcst_outputs);
+	printf("\n");
+	for (int i = 1; i <= 4; i++) { printf("2:%d:%s\n", i, mv_alloc_format_val(&pvars[i])); }
+	mu_assert_lf(mv_is_absent(&pvars[0]));
+	mu_assert_lf(mv_is_absent(&pvars[1]));
+	mu_assert_lf(mv_equals_si(&pvars[2], imv(2)));
+	mu_assert_lf(mv_equals_si(&pvars[3], smv("ijkl")));
+	mu_assert_lf(mv_is_absent(&pvars[4]));
 
+	// Push record 3
+	pvariables->pctx->nr = 3;
+	mlr_dsl_cst_handle_top_level_statement_block(pcst->pmain_block, pvariables, pcst_outputs);
+	printf("\n");
+	for (int i = 1; i <= 4; i++) { printf("3:%d:%s\n", i, mv_alloc_format_val(&pvars[i])); }
+	mu_assert_lf(mv_is_absent(&pvars[0]));
+	mu_assert_lf(mv_is_absent(&pvars[1]));
+	mu_assert_lf(mv_equals_si(&pvars[2], imv(3)));
+	mu_assert_lf(mv_equals_si(&pvars[3], imv(3000)));
+	mu_assert_lf(mv_is_absent(&pvars[4]));
+
+	// Push record 4
+	pvariables->pctx->nr = 4;
+	mlr_dsl_cst_handle_top_level_statement_block(pcst->pmain_block, pvariables, pcst_outputs);
+	printf("\n");
+	for (int i = 1; i <= 4; i++) { printf("4:%d:%s\n", i, mv_alloc_format_val(&pvars[i])); }
+	mu_assert_lf(mv_is_absent(&pvars[0]));
+	mu_assert_lf(mv_equals_si(&pvars[1], imv(3025)));
+	mu_assert_lf(mv_equals_si(&pvars[2], imv(3025)));
+	mu_assert_lf(mv_equals_si(&pvars[3], imv(11)));
+	mu_assert_lf(mv_equals_si(&pvars[3], imv(11)));
+
+	printf("%s\n", sep);
+	return NULL;
+}
 
 // ================================================================
 static char * run_all_tests() {
 	mu_run_test(test_empty);
 	mu_run_test(test_top_level_locals);
 	mu_run_test(test_top_level_clears);
+	mu_run_test(test_placements);
 	return 0;
 }
 
