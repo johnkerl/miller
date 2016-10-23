d748947c03a7fc66bb791c3b1e402d789f56b3ff
John Kerl <kerl.john.r@gmail.com>
2016-10-22 22:51:43 -0400

    type-decl iterate

--- c/mapping/mlr_dsl_cst.h
+++ c/mapping/mlr_dsl_cst.h
@@ -3,6 +3,7 @@
 
 #include "cli/mlrcli.h"
 #include "mapping/mlr_dsl_ast.h"
+#include "containers/type_decl.h"
 #include "containers/lhmsmv.h"
 #include "containers/local_stack.h"
 #include "containers/loop_stack.h"
@@ -50,24 +51,6 @@
 // The exceptions being, of course, assignment to/from srec is disallowed for begin/end statements since those occur
 // before/after stream processing, respectively.
 // ================================================================
-
-// ----------------------------------------------------------------
-// xxx to separate .h/.c files?
-// These use the MT defines from mlrval.h
-#define TYPE_MASK_ERROR   (1 << MT_ERROR)
-#define TYPE_MASK_ABSENT  (1 << MT_ABSENT)
-#define TYPE_MASK_EMPTY   (1 << MT_EMPTY)
-#define TYPE_MASK_STRING  (1 << MT_STRING)
-#define TYPE_MASK_INT     (1 << MT_INT)
-#define TYPE_MASK_FLOAT   (1 << MT_FLOAT)
-#define TYPE_MASK_BOOLEAN (1 << MT_BOOL)
-#define TYPE_MASK_MAP     (1 << MT_DIM) // xxx for later. xxx note map-types are outside of mlrval.h.
-
-#define TYPE_MASK_NUMERIC (TYPE_MASK_INT | TYPE_MASK_FLOAT)
-#define TYPE_MASK_PRESENT (TYPE_MASK_EMPTY | TYPE_MASK_STRING | TYPE_MASK_INT | TYPE_MASK_FLOAT | TYPE_MASK_BOOLEAN)
-#define TYPE_MASK_ANY     (~0)
-
-char* type_mask_to_desc(int type_mask);
 
 // ----------------------------------------------------------------
 // Two-pass stack allocator which operates on the block-structured AST
