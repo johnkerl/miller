0a3bf63eadee1ccecc4c95920c77ab2ee45866c2
John Kerl <kerl.john.r@gmail.com>
2016-10-27 09:33:31 -0400

    neaten

--- c/containers/type_decl.h
+++ c/containers/type_decl.h
@@ -4,17 +4,17 @@
 #include "../containers/mlrval.h"
 
 // ----------------------------------------------------------------
-// These use the MT defines from mlrval.h
+// These use the MT defines from mlrval.h, except that map-types (mlhmmv.h)
+// are outside of mlrval.h.
 #define TYPE_MASK_ERROR    (1 << MT_ERROR)
 #define TYPE_MASK_ABSENT   (1 << MT_ABSENT)
 #define TYPE_MASK_EMPTY    (1 << MT_EMPTY)
 #define TYPE_MASK_STRING  ((1 << MT_STRING) | (1 << MT_EMPTY))
 #define TYPE_MASK_INT      (1 << MT_INT)
 #define TYPE_MASK_FLOAT    (1 << MT_FLOAT)
-#define TYPE_MASK_BOOLEAN  (1 << MT_BOOLEAN)
-#define TYPE_MASK_MAP      (1 << MT_DIM) // xxx for later. xxx note map-types are outside of mlrval.h.
-
 #define TYPE_MASK_NUMERIC (TYPE_MASK_INT | TYPE_MASK_FLOAT)
+#define TYPE_MASK_BOOLEAN  (1 << MT_BOOLEAN)
+#define TYPE_MASK_MAP      (1 << MT_DIM)
 #define TYPE_MASK_ANY     (~0)
 
 // ----------------------------------------------------------------
