9fc0f90f9d0f32e2a17f95d007e4ffdc32a2e116
John Kerl <kerl.john.r@gmail.com>
2016-12-04 16:24:25 -0500

    todo

--- c/containers/xvfuncs.h
+++ c/containers/xvfuncs.h
@@ -27,28 +27,39 @@ typedef mlhmmv_xvalue_t mv_ternary_func_t(
 	mlhmmv_xvalue_t* pval2,
 	mlhmmv_xvalue_t* pval3);
 
-//// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// xxx to do
+
+//mlhmmv_xvalue_t b_x_ispresent_xfunc(mlhmmv_xvalue_t* pval1);
+//mlhmmv_xvalue_t b_x_isabsent_xfunc(mlhmmv_xvalue_t* pval1);
+//mlhmmv_xvalue_t b_x_ismap_xfunc(mlhmmv_xvalue_t* pval1);
+//mlhmmv_xvalue_t b_x_isscalar_xfunc(mlhmmv_xvalue_t* pval1);
+
+// isnull
+// isnotnull
+// isabsent
+// ispresent
+// isempty
+// isnotempty
+// isnumeric
+// isint
+// isfloat
+// isbool
+// isboolean
+// isstring
+
+//mlhmmv_xvalue_t b_m_haskey_xfunc(mlhmmv_xvalue_t* pval1);
+//mlhmmv_xvalue_t s_x_typeof_xfunc(mlhmmv_xvalue_t* pval1);
+//mlhmmv_xvalue_t i_m_length_xfunc(mlhmmv_xvalue_t* pval1);
+//mlhmmv_xvalue_t i_m_depth_xfunc(mlhmmv_xvalue_t* pval1);
+//mlhmmv_xvalue_t i_m_deepcount_xfunc(mlhmmv_xvalue_t* pval1);
+//mlhmmv_xvalue_t m_mm_mapsum_xfunc(mlhmmv_xvalue_t* pval1, mlhmmv_xvalue_t* pval2);
+//  - binary m,s->s JOIN
+//  - binary s,s->m SPLIT
+//mlhmmv_xvalue_t m_mm_mapdiff_xfunc(mlhmmv_xvalue_t* pval1, mlhmmv_xvalue_t* pval2);
+
 //static inline mlhmmv_xvalue_t b_b_not_func(mlhmmv_xvalue_t* pval1) {
 //	return mv_from_bool(!pval1->u.boolv);
 //}
-//
-//static inline mlhmmv_xvalue_t b_bb_or_func(mlhmmv_xvalue_t* pval1, mlhmmv_xvalue_t* pval2) {
-//	return mv_from_bool(pval1->u.boolv || pval2->u.boolv);
-//}
-//static inline mlhmmv_xvalue_t b_bb_and_func(mlhmmv_xvalue_t* pval1, mlhmmv_xvalue_t* pval2) {
-//	return mv_from_bool(pval1->u.boolv && pval2->u.boolv);
-//}
-//static inline mlhmmv_xvalue_t b_bb_xor_func(mlhmmv_xvalue_t* pval1, mlhmmv_xvalue_t* pval2) {
-//	return mv_from_bool(pval1->u.boolv ^ pval2->u.boolv);
-//}
-
-//  - unary m->b ISPRESENT ISABSENT ISMAP ISSCALAR
-//  - unary m->b HASKEY
-//  - unary m->i LENGTH DEPTH DEEPCOUNT
-//  - unary x->s TYPEOF
-//  - binary m,s->s JOIN
-//
-//  - binary s,s->m SPLIT
-//  - binary/variadic m,m->m MAPSUM MAPDIFF
 
 #endif // XVFUNCS_H
