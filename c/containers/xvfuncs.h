37337f3b1a0c64723e7300cb38e441656c01021d
John Kerl <kerl.john.r@gmail.com>
2016-12-04 16:35:09 -0500

    xvfuncs iterate

--- c/containers/xvfuncs.h
+++ c/containers/xvfuncs.h
@@ -5,6 +5,8 @@
 // Functions on extended values, namely, mlrvals/hashmaps.
 // ================================================================
 
+// xxx make the xvfuncs API entirely in terms of boxed_xval_t's?
+
 #include "../lib/mlrutil.h"
 #include "../containers/mlhmmv.h"
 
@@ -16,24 +18,51 @@ typedef mlhmmv_xvalue_t mv_variadic_func_t(
 typedef mlhmmv_xvalue_t mv_zary_func_t();
 
 typedef mlhmmv_xvalue_t mv_unary_func_t(
-	mlhmmv_xvalue_t* pval1);
+	mlhmmv_xvalue_t* pxval1);
 
 typedef mlhmmv_xvalue_t mv_binary_func_t(
-	mlhmmv_xvalue_t* pval1,
-	mlhmmv_xvalue_t* pval2);
+	mlhmmv_xvalue_t* pxval1,
+	mlhmmv_xvalue_t* pxval2);
 
 typedef mlhmmv_xvalue_t mv_ternary_func_t(
-	mlhmmv_xvalue_t* pval1,
-	mlhmmv_xvalue_t* pval2,
-	mlhmmv_xvalue_t* pval3);
+	mlhmmv_xvalue_t* pxval1,
+	mlhmmv_xvalue_t* pxval2,
+	mlhmmv_xvalue_t* pxval3);
 
 // ----------------------------------------------------------------
 // xxx to do
 
-//mlhmmv_xvalue_t b_x_ispresent_xfunc(mlhmmv_xvalue_t* pval1);
-//mlhmmv_xvalue_t b_x_isabsent_xfunc(mlhmmv_xvalue_t* pval1);
-//mlhmmv_xvalue_t b_x_ismap_xfunc(mlhmmv_xvalue_t* pval1);
-//mlhmmv_xvalue_t b_x_isscalar_xfunc(mlhmmv_xvalue_t* pval1);
+static inline mlhmmv_xvalue_t b_x_ispresent_xfunc(mlhmmv_xvalue_t* pxval1) {
+	return mlhmmv_xvalue_wrap_terminal(
+		mv_from_bool(
+			!pxval1->is_terminal || mv_is_present(&pxval1->terminal_mlrval)
+		)
+	);
+}
+
+static inline mlhmmv_xvalue_t b_x_isabsent_xfunc(mlhmmv_xvalue_t* pxval1) {
+	return mlhmmv_xvalue_wrap_terminal(
+		mv_from_bool(
+			pxval1->is_terminal && mv_is_absent(&pxval1->terminal_mlrval)
+		)
+	);
+}
+
+static inline mlhmmv_xvalue_t b_x_ismap_xfunc(mlhmmv_xvalue_t* pxval1) {
+	return mlhmmv_xvalue_wrap_terminal(
+		mv_from_bool(
+			!pxval1->is_terminal
+		)
+	);
+}
+
+static inline mlhmmv_xvalue_t b_x_isscalar_xfunc(mlhmmv_xvalue_t* pxval1) {
+	return mlhmmv_xvalue_wrap_terminal(
+		mv_from_bool(
+			pxval1->is_terminal && mv_is_present(&pxval1->terminal_mlrval)
+		)
+	);
+}
 
 // isnull
 // isnotnull
@@ -48,18 +77,18 @@ typedef mlhmmv_xvalue_t mv_ternary_func_t(
 // isboolean
 // isstring
 
-//mlhmmv_xvalue_t b_m_haskey_xfunc(mlhmmv_xvalue_t* pval1);
-//mlhmmv_xvalue_t s_x_typeof_xfunc(mlhmmv_xvalue_t* pval1);
-//mlhmmv_xvalue_t i_m_length_xfunc(mlhmmv_xvalue_t* pval1);
-//mlhmmv_xvalue_t i_m_depth_xfunc(mlhmmv_xvalue_t* pval1);
-//mlhmmv_xvalue_t i_m_deepcount_xfunc(mlhmmv_xvalue_t* pval1);
-//mlhmmv_xvalue_t m_mm_mapsum_xfunc(mlhmmv_xvalue_t* pval1, mlhmmv_xvalue_t* pval2);
+//mlhmmv_xvalue_t b_m_haskey_xfunc(mlhmmv_xvalue_t* pxval1);
+//mlhmmv_xvalue_t s_x_typeof_xfunc(mlhmmv_xvalue_t* pxval1);
+//mlhmmv_xvalue_t i_m_length_xfunc(mlhmmv_xvalue_t* pxval1);
+//mlhmmv_xvalue_t i_m_depth_xfunc(mlhmmv_xvalue_t* pxval1);
+//mlhmmv_xvalue_t i_m_deepcount_xfunc(mlhmmv_xvalue_t* pxval1);
+//mlhmmv_xvalue_t m_mm_mapsum_xfunc(mlhmmv_xvalue_t* pxval1, mlhmmv_xvalue_t* pxval2);
 //  - binary m,s->s JOIN
 //  - binary s,s->m SPLIT
-//mlhmmv_xvalue_t m_mm_mapdiff_xfunc(mlhmmv_xvalue_t* pval1, mlhmmv_xvalue_t* pval2);
+//mlhmmv_xvalue_t m_mm_mapdiff_xfunc(mlhmmv_xvalue_t* pxval1, mlhmmv_xvalue_t* pxval2);
 
-//static inline mlhmmv_xvalue_t b_b_not_func(mlhmmv_xvalue_t* pval1) {
-//	return mv_from_bool(!pval1->u.boolv);
+//static inline mlhmmv_xvalue_t b_b_not_func(mlhmmv_xvalue_t* pxval1) {
+//	return mv_from_bool(!pxval1->u.boolv);
 //}
 
 #endif // XVFUNCS_H
