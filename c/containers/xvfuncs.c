7ba255f57f03a0b33d3e2103a98521fd959f6479
John Kerl <kerl.john.r@gmail.com>
2016-12-05 18:38:49 -0500

    mapsum 1st light

--- c/containers/xvfuncs.c
+++ c/containers/xvfuncs.c
@@ -32,7 +32,20 @@ boxed_xval_t i_x_length_xfunc(boxed_xval_t* pxval1) {
 
 // ----------------------------------------------------------------
 boxed_xval_t variadic_mapsum_xfunc(boxed_xval_t* pbxvals, int nxvals) {
-	return box_ephemeral_val(mv_absent()); // xxx stub
+	// xxx to-do optmization: transfer arg 1 if it's ephemeral
+	mlhmmv_xvalue_t sum = mlhmmv_xvalue_alloc_empty_map();
+	for (int i = 0; i < nxvals; i++) {
+		if (pbxvals[i].xval.is_terminal)
+			continue;
+		mlhmmv_level_t* plevel = pbxvals[i].xval.pnext_level;
+		for (mlhmmv_level_entry_t* pe = plevel->phead; pe != NULL; pe = pe->pnext) {
+			// xxx do refs/copies correctly
+			mlhmmv_xvalue_t xval_copy = mlhmmv_xvalue_copy(&pe->level_xvalue);
+			sllmve_t e = (sllmve_t) { .value = pe->level_key, .free_flags = 0, .pnext = NULL };
+			mlhmmv_level_put_xvalue(sum.pnext_level, &e, &xval_copy);
+		}
+	}
+	return box_ephemeral_xval(sum);
 }
 
 // ----------------------------------------------------------------
