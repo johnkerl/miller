a0d5837df8114cc5c4c2b5abfd7a9adfa753ced4
John Kerl <kerl.john.r@gmail.com>
2016-12-06 08:53:17 -0500

    mapdiff 1st light

--- c/containers/xvfuncs.c
+++ c/containers/xvfuncs.c
@@ -50,5 +50,33 @@ boxed_xval_t variadic_mapsum_xfunc(boxed_xval_t* pbxvals, int nxvals) {
 
 // ----------------------------------------------------------------
 boxed_xval_t variadic_mapdiff_xfunc(boxed_xval_t* pbxvals, int nxvals) {
-	return box_ephemeral_val(mv_absent()); // xxx stub
+	mlhmmv_xvalue_t diff = mlhmmv_xvalue_alloc_empty_map();
+	if (nxvals == 0) {
+		return box_ephemeral_xval(diff);
+	}
+	// xxx to-do optmization: transfer arg 1 if it's ephemeral
+
+	// xxx methodize
+	int i = 0;
+	if (!pbxvals[i].xval.is_terminal) {
+		mlhmmv_level_t* plevel = pbxvals[i].xval.pnext_level;
+		for (mlhmmv_level_entry_t* pe = plevel->phead; pe != NULL; pe = pe->pnext) {
+			// xxx do refs/copies correctly
+			mlhmmv_xvalue_t xval_copy = mlhmmv_xvalue_copy(&pe->level_xvalue);
+			sllmve_t e = (sllmve_t) { .value = pe->level_key, .free_flags = 0, .pnext = NULL };
+			mlhmmv_level_put_xvalue(diff.pnext_level, &e, &xval_copy);
+		}
+	}
+
+	for (i = 1; i < nxvals; i++) {
+		if (!pbxvals[i].xval.is_terminal) {
+			mlhmmv_level_t* plevel = pbxvals[i].xval.pnext_level;
+			for (mlhmmv_level_entry_t* pe = plevel->phead; pe != NULL; pe = pe->pnext) {
+				sllmve_t e = (sllmve_t) { .value = pe->level_key, .free_flags = 0, .pnext = NULL };
+				mlhmmv_level_remove(diff.pnext_level, &e);
+			}
+		}
+	}
+
+	return box_ephemeral_xval(diff);
 }
