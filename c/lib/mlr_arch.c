8b296cf342a5c2e697e14924729c0b9fcb6d4e76
John Kerl <kerl.john.r@gmail.com>
2017-04-07 23:45:08 -0400

    windows/time iterate

--- c/lib/mlr_arch.c
+++ c/lib/mlr_arch.c
@@ -6,6 +6,9 @@
 #include "mlrutil.h"
 #include "netbsd_strptime.h"
 
+// For some Linux distros, in spite of including time.h:
+char *strptime(const char *s, const char *format, struct tm *ptm);
+
 // ----------------------------------------------------------------
 int mlr_arch_setenv(const char *name, const char *value) {
 #ifdef MLR_ON_MSYS2
@@ -29,8 +32,8 @@ int mlr_arch_unsetenv(const char *name) {
 // ----------------------------------------------------------------
 char *mlr_arch_strptime(const char *s, const char *format, struct tm *ptm) {
 #ifdef MLR_ON_MSYS2
-	return strptime(*s, format, ptm);
-#else
 	return netbsd_strptime(s, format, ptm);
+#else
+	return strptime(s, format, ptm);
 #endif
 }
