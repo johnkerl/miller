7ffc561f8eda654614fcef664786cf5c150e3b4f
John Kerl <kerl.john.r@gmail.com>
2017-04-07 22:20:26 -0400

    windows/time iterate

--- c/experimental/try.c
+++ c/experimental/try.c
@@ -4,94 +4,41 @@
 #include <time.h>
 #include <sys/time.h>
 
-// For some Linux distros, in spite of including time.h:
-char *strptime(const char *s, const char *format, struct tm *ptm);
-
-// ----------------------------------------------------------------
-char * mlr_strptime(const char *buf, const char *format, struct tm *timeptr);
-size_t mlr_strftime(char *restrict s, size_t maxsize, const char *restrict format,
-	const struct tm *restrict timeptr);
-
-// ----------------------------------------------------------------
 #define ISO8601_TIME_FORMAT "%Y-%m-%dT%H:%M:%SZ"
-
-// ----------------------------------------------------------------
-// seconds since the epoch
-double get_systime() {
-	struct timeval tv = { .tv_sec = 0, .tv_usec = 0 };
-	(void)gettimeofday(&tv, NULL);
-	return (double)tv.tv_sec + (double)tv.tv_usec * 1e-6;
-}
-
-// ----------------------------------------------------------------
-// See the GNU timegm manpage -- this is what it does.
-time_t mlr_timegm(struct tm* ptm) {
-	time_t ret;
-	char* tz;
-
-	tz = getenv("TZ");
-	setenv("TZ", "GMT0", 1);
-	tzset();
-	ret = mktime(ptm);
-	if (tz) {
-		setenv("TZ", tz, 1);
-	} else {
-		unsetenv("TZ");
-	}
-	tzset();
-	return ret;
-}
-
-// ----------------------------------------------------------------
 #define NZBUFLEN 63
-char* mlr_alloc_time_string_from_seconds(time_t seconds, char* format, int use_new) {
-	struct tm tm;
-	struct tm *ptm = gmtime_r(&seconds, &tm);
-	char* string = malloc(NZBUFLEN + 1);
-	int written_length = use_new
-		? mlr_strftime(string, NZBUFLEN, format, ptm)
-		: strftime(string, NZBUFLEN, format, ptm);
-	if (written_length > NZBUFLEN || written_length == 0) {
-		fprintf(stderr, "(new=%d) Could not strftime(\"%s\", \"%s\").\n", use_new, string, format);
-	}
-
-	return string;
-}
-
-// ----------------------------------------------------------------
-time_t mlr_seconds_from_time_string(char* string, char* format, int use_new) {
-	struct tm tm;
-	memset(&tm, 0, sizeof(tm));
-	char* retval = use_new
-		? mlr_strptime(string, format, &tm)
-		: strptime(string, format, &tm);
-	if (retval == NULL) {
-		fprintf(stderr, "(new=%d) Could not strptime(\"%s\", \"%s\").\n", use_new, string, format);
-	}
-	return mlr_timegm(&tm);
-}
+//char *strptime(const char *s, const char *format, struct tm *ptm);
 
 // ----------------------------------------------------------------
 int main(int argc, char** argv) {
-	time_t t = 1491399761;
-	char* format = ISO8601_TIME_FORMAT;
-	if (argc == 3) {
-		int i;
-		if (sscanf(argv[1], "%d", &i) != 1) {
-			fprintf(stderr, "b04k!\n");
-			exit(1);
-		}
-		t = (time_t) i;
-		format = argv[2];
+	time_t strftime_input = 1491399761;
+
+	struct tm tm = *gmtime(&strftime_input);
+	char* strftime_output = malloc(NZBUFLEN + 1);
+	int written_length = strftime(strftime_output, NZBUFLEN, ISO8601_TIME_FORMAT, &tm);
+	if (written_length > NZBUFLEN || written_length == 0) {
+		strcpy(strftime_output, "strftime failed");
 	}
-	char*  f0 = mlr_alloc_time_string_from_seconds(t, format, 0);
-	char*  f1 = mlr_alloc_time_string_from_seconds(t, format, 1);
-	time_t p0 = mlr_seconds_from_time_string(f0, format, 0);
-	time_t p1 = mlr_seconds_from_time_string(f0, format, 1);
-	printf("t  = %d\n", (int)t);
-	printf("f0 = %s\n", f0);
-	printf("f1 = %s\n", f1);
-	printf("p0 = %d\n", (int)p0);
-	printf("p1 = %d\n", (int)p1);
+	printf("strftime: %d -> %s\n", (int)strftime_input, strftime_output);
+
+	char* strptime_input = "2017-03-04T05:06:07Z";
+	char* strptime_output = strptime(strptime_input, ISO8601_TIME_FORMAT, &tm);
+	if (strptime_output == NULL) {
+		printf("Could not strptime(\"%s\", \"%s\").\n", strptime_input, ISO8601_TIME_FORMAT);
+	} else {
+		printf("strptime: %s ->\n", strptime_input);
+		printf("  tm_sec    = %d\n",  tm.tm_sec);
+		printf("  tm_min    = %d\n",  tm.tm_min);
+		printf("  tm_hour   = %d\n",  tm.tm_hour);
+		printf("  tm_mday   = %d\n",  tm.tm_mday);
+		printf("  tm_mon    = %d\n",  tm.tm_mon);
+		printf("  tm_year   = %d\n",  tm.tm_year);
+		printf("  tm_wday   = %d\n",  tm.tm_wday);
+		printf("  tm_yday   = %d\n",  tm.tm_yday);
+		printf("  tm_isdst  = %d\n",  tm.tm_isdst);
+		printf("  tm_zone   = %s\n",  tm.tm_zone);
+		printf("  tm_gmtoff = %ld\n", tm.tm_gmtoff);
+		printf("  remainder = \"%s\"\n", strptime_output);
+	}
+
 	return 0;
 }
