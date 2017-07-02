148cb9b21ab5a20c3d865339ddac08cb9df2aa63
John Kerl <kerl.john.r@gmail.com>
2017-07-02 13:52:29 -0400

    neaten

--- msys2-build.sh
+++ msys2-build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo ================================================================
-#### xxx temp
-#### sed 's/-lm/-lm -lpcreposix/' c/Makefile.am > temp; mv temp c/Makefile.am
-#### xxx temp
-#### sed 's/-lm/-lm -lpcreposix/' c/unit_test/Makefile.am > temp; mv temp c/unit_test/Makefile.am
+
+# Note: I need this on my Windows machine with MSYS2 but it causes an
+# error on Appveyor.
+# sed 's/-lm/-lm -lpcreposix/' c/Makefile.am > temp; mv temp c/Makefile.am
+# sed 's/-lm/-lm -lpcreposix/' c/unit_test/Makefile.am > temp; mv temp c/unit_test/Makefile.am
+
 sed 's/#undef MLR_ON_MSYS2/#define MLR_ON_MSYS2/' c/lib/mlr_arch.h > temp; mv temp c/lib/mlr_arch.h
 
 echo ================================================================
