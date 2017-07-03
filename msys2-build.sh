55fff2ec68591782cfe80604bd3567697941bcad
John Kerl <kerl.john.r@gmail.com>
2017-07-02 21:11:22 -0400

    appveyor iterate

--- msys2-build.sh
+++ msys2-build.sh
@@ -7,9 +7,6 @@ echo ================================================================
 # sed 's/-lm/-lm -lpcreposix/' c/Makefile.am > temp; mv temp c/Makefile.am
 # sed 's/-lm/-lm -lpcreposix/' c/unit_test/Makefile.am > temp; mv temp c/unit_test/Makefile.am
 
-sed 's/-lm/-lm -static/' c/Makefile.am > temp; mv temp c/Makefile.am
-sed 's/-lm/-lm -static/' c/unit_test/Makefile.am > temp; mv temp c/unit_test/Makefile.am
-
 sed 's/#undef MLR_ON_MSYS2/#define MLR_ON_MSYS2/' c/lib/mlr_arch.h > temp; mv temp c/lib/mlr_arch.h
 
 echo ================================================================
