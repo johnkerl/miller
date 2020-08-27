841bac7767d49fceabaa94e2c10a4e21959f97dd
John Kerl <kerl.john.r@gmail.com>
2020-08-27 08:47:22 -0400

    csv reader iterate

---
+++ go/src/miller/input/record_reader_csv.go
@@ -0,0 +1,85 @@
+package input
+
+import (
+	// System:
+	"encoding/csv"
+	"io"
+	"os"
+	// Miller:
+	"miller/containers"
+)
+
+type RecordReaderCSV struct {
+	// TODO: parameterize
+	//ifs string
+	//irs string
+	needHeader bool
+	header     []string
+}
+
+func NewRecordReaderCSV( /*ifs string, ips string*/ ) *RecordReaderCSV {
+	return &RecordReaderCSV{
+		true,
+		nil,
+		//ifs,
+		//irs,
+	}
+}
+
+func (this *RecordReaderCSV) Read(
+	filenames []string,
+	inrecs chan<- *containers.Lrec,
+	echan chan error,
+) {
+	// TODO: loop over filenames
+	// TODO: handle empty filenames array as read-from-stdin
+	filename := filenames[0]
+
+	handle, err := os.Open(filename)
+	if err != nil {
+		echan <- err
+	}
+
+	csvReader := csv.NewReader(handle)
+
+	for {
+		if this.needHeader {
+			// TODO: make this a helper function
+			record, err := csvReader.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				echan <- err
+				return
+			}
+			this.header = record
+
+			this.needHeader = false
+		}
+
+		record, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			echan <- err
+			return
+		}
+
+		lrec := containers.LrecAlloc()
+
+		// TODO: check for length mismatches
+		n := len(this.header)
+		for i := 0; i < n; i++ {
+			key := this.header[i]
+			value := record[i]
+			// to do: avoid re-walk ...
+			lrec.Put(&key, &value)
+		}
+
+		inrecs <- lrec
+	}
+
+	inrecs <- nil // signals end of input record stream
+}
--- go/src/miller/input/record_reader_factory.go
+++ go/src/miller/input/record_reader_factory.go
@@ -2,6 +2,8 @@ package input
 
 func Create(inputFormatName string) RecordReader {
 	switch inputFormatName {
+	case "csv":
+		return NewRecordReaderCSV() // TODO: parameterize
 	case "dkvp":
 		return NewRecordReaderDKVP(",", "=") // TODO: parameterize
 	default:
