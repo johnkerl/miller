151d96cd2b9ff77865ddb72b89d601c56bc9e3f9
John Kerl <kerl.john.r@gmail.com>
2020-08-30 12:09:07 -0400

    grammar iterate: indirect field name

--- go/src/miller/dsl/ast.go
+++ go/src/miller/dsl/ast.go
@@ -13,6 +13,7 @@ const (
 	NodeTypeToken          = "Token"
 	NodeTypeAssignment     = "Assignment"
 	NodeTypeOperator       = "Operator"
+	NodeTypeFieldName      = "FieldName"
 )
 
 // ----------------------------------------------------------------
