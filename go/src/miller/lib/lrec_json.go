8ea0f74831588b5337feb28e7afb8ec9f4ecd5ca
John Kerl <kerl.john.r@gmail.com>
2020-09-04 12:58:33 -0400

    split up lrec.go

---
+++ go/src/miller/lib/lrec_json.go
@@ -0,0 +1,8 @@
+package lib
+
+// TODO: implement json.Marshaler and json.Unmarshaler interfaces -- either at
+// the Mlrval level or the Lrec level (or both ...)
+
+import (
+// "encoding/json"
+)
---
+++ go/src/miller/lib/lrec_print.go
@@ -0,0 +1,99 @@
+package lib
+
+import (
+	"bytes"
+	"os"
+)
+
+// ----------------------------------------------------------------
+func (this *Lrec) Print() {
+	this.Fprint(os.Stdout)
+}
+func (this *Lrec) Fprint(file *os.File) {
+	var buffer bytes.Buffer // 5x faster than fmt.Print() separately
+	for pe := this.Head; pe != nil; pe = pe.Next {
+		buffer.WriteString(*pe.Key)
+		buffer.WriteString("=")
+		buffer.WriteString(pe.Value.String())
+		if pe.Next != nil {
+			buffer.WriteString(",")
+		}
+	}
+	buffer.WriteString("\n")
+	(*file).WriteString(buffer.String())
+}
+
+//// ----------------------------------------------------------------
+//void lrec_dump(Lrec* prec) {
+//	lrec_dump_fp(prec, stdout);
+//}
+
+//void lrec_dump_fp(Lrec* prec, FILE* fp) {
+//	if (prec == NULL) {
+//		fprintf(fp, "NULL\n");
+//		return;
+//	}
+//	fprintf(fp, "field_count = %d\n", prec->field_count);
+//	fprintf(fp, "| Head: %16p | Tail %16p\n", prec->Head, prec->Tail);
+//	for (lrecEntry* pe = prec->Head; pe != NULL; pe = pe->Next) {
+//		const char* key_string = (pe == NULL) ? "none" :
+//			pe->key == NULL ? "null" :
+//			pe->key;
+//		const char* value_string = (pe == NULL) ? "none" :
+//			pe->value == NULL ? "null" :
+//			pe->value;
+//		fprintf(fp,
+//		"| prev: %16p curr: %16p next: %16p | key: %12s | value: %12s |\n",
+//			pe->Prev, pe, pe->Next,
+//			key_string, value_string);
+//	}
+//}
+
+//void lrec_dump_titled(char* msg, Lrec* prec) {
+//	printf("%s:\n", msg);
+//	lrec_dump(prec);
+//	printf("\n");
+//}
+
+//void lrec_print(Lrec* prec) {
+//	FILE* output_stream = stdout;
+//	char ors = '\n';
+//	char ofs = ',';
+//	char ops = '=';
+//	if (prec == NULL) {
+//		fputs("NULL", output_stream);
+//		fputc(ors, output_stream);
+//		return;
+//	}
+//	int nf = 0;
+//	for (lrecEntry* pe = prec->Head; pe != NULL; pe = pe->Next) {
+//		if (nf > 0)
+//			fputc(ofs, output_stream);
+//		fputs(pe->key, output_stream);
+//		fputc(ops, output_stream);
+//		fputs(pe->value, output_stream);
+//		nf++;
+//	}
+//	fputc(ors, output_stream);
+//}
+
+//char* lrec_sprint(Lrec* prec, char* ors, char* ofs, char* ops) {
+//	string_builder_t* psb = sb_alloc(SB_ALLOC_LENGTH);
+//	if (prec == NULL) {
+//		sb_append_string(psb, "NULL");
+//	} else {
+//		int nf = 0;
+//		for (lrecEntry* pe = prec->Head; pe != NULL; pe = pe->Next) {
+//			if (nf > 0)
+//				sb_append_string(psb, ofs);
+//			sb_append_string(psb, pe->key);
+//			sb_append_string(psb, ops);
+//			sb_append_string(psb, pe->value);
+//			nf++;
+//		}
+//		sb_append_string(psb, ors);
+//	}
+//	char* rv = sb_finish(psb);
+//	sb_free(psb);
+//	return rv;
+//}
