b577a50955294fe73ab1ffa07996b421639d668c
John Kerl <kerl.john.r@gmail.com>
2020-09-23 00:41:22 -0400

    todo

--- go/src/miller/dsl/cst/for.go
+++ go/src/miller/dsl/cst/for.go
@@ -25,7 +25,7 @@ func NewForLoopKeyOnlyNode(
 // ----------------------------------------------------------------
 // Sample AST:
 
-// mlr -n put -v for (k in $*) { emit { k : k} }
+// mlr -n put -v for (k in $*) { emit { k : k } }
 // DSL EXPRESSION:
 // for (k in $*) { emit { k : k} }
 // RAW AST:
@@ -42,11 +42,26 @@ func NewForLoopKeyOnlyNode(
 
 func BuildForLoopKeyOnlyNode(astNode *dsl.ASTNode) (*ForLoopKeyOnlyNode, error) {
 	lib.InternalCodingErrorIf(astNode.Type != dsl.NodeTypeForLoopKeyOnly)
+	lib.InternalCodingErrorIf(len(astNode.Children) != 3)
+
+	// TODO
+
+	// error if loop-over node isn't Mappable (inasmuch as can be detected at CST-build time)
 
 	return NewForLoopKeyOnlyNode(), nil
 }
 
 // ----------------------------------------------------------------
 func (this *ForLoopKeyOnlyNode) Execute(state *State) error {
+	// TODO
+
+	// absent/error handling
+	// error if loop-over node isn't a mlrmap
+	// loop over map keys
+	// * make new stack frame, binding the localvar name to its .Copy()
+	// * push the stack frame
+	// * statement-block execute
+	// * pop the stack frame
+
 	return nil
 }
