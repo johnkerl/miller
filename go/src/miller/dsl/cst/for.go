3d6e4c55289f1b85fd1c8866000916b59014da35
John Kerl <kerl.john.r@gmail.com>
2020-09-23 20:58:17 -0400

    for-loop over arrays as well as maps

--- go/src/miller/dsl/cst/for.go
+++ go/src/miller/dsl/cst/for.go
@@ -12,25 +12,6 @@ import (
 // This is for various flavors of for-loop
 // ================================================================
 
-// ================================================================
-type ForLoopKeyOnlyNode struct {
-	keyVariableName    string
-	mapNode            IEvaluable
-	statementBlockNode *StatementBlockNode
-}
-
-func NewForLoopKeyOnlyNode(
-	keyVariableName string,
-	mapNode IEvaluable,
-	statementBlockNode *StatementBlockNode,
-) *ForLoopKeyOnlyNode {
-	return &ForLoopKeyOnlyNode{
-		keyVariableName,
-		mapNode,
-		statementBlockNode,
-	}
-}
-
 // ----------------------------------------------------------------
 // Sample AST:
 
@@ -49,12 +30,12 @@ func NewForLoopKeyOnlyNode(
 //                         * LocalVariable "k"
 //                         * LocalVariable "k"
 
-func BuildForLoopKeyOnlyNode(astNode *dsl.ASTNode) (*ForLoopKeyOnlyNode, error) {
+func BuildForLoopKeyOnlyNode(astNode *dsl.ASTNode) (*ForLoopKeyValueNode, error) {
 	lib.InternalCodingErrorIf(astNode.Type != dsl.NodeTypeForLoopKeyOnly)
 	lib.InternalCodingErrorIf(len(astNode.Children) != 3)
 
 	keyVariableASTNode := astNode.Children[0]
-	mapASTNode := astNode.Children[1]
+	indexableASTNode := astNode.Children[1]
 	blockASTNode := astNode.Children[2]
 
 	lib.InternalCodingErrorIf(keyVariableASTNode.Type != dsl.NodeTypeLocalVariable)
@@ -63,7 +44,8 @@ func BuildForLoopKeyOnlyNode(astNode *dsl.ASTNode) (*ForLoopKeyOnlyNode, error) 
 
 	// TODO: error if loop-over node isn't Mappable (inasmuch as can be
 	// detected at CST-build time)
-	mapNode, err := BuildEvaluableNode(mapASTNode)
+	// TODO: support arrays too.
+	indexableNode, err := BuildEvaluableNode(indexableASTNode)
 	if err != nil {
 		return nil, err
 	}
@@ -73,71 +55,32 @@ func BuildForLoopKeyOnlyNode(astNode *dsl.ASTNode) (*ForLoopKeyOnlyNode, error) 
 		return nil, err
 	}
 
-	return NewForLoopKeyOnlyNode(
+	return NewForLoopKeyValueNode(
 		keyVariableName,
-		mapNode,
+		"", // No value-variable here, but re-use the code
+		indexableNode,
 		statementBlockNode,
 	), nil
-}
-
-// ----------------------------------------------------------------
-func (this *ForLoopKeyOnlyNode) Execute(state *State) error {
-
-	mlrval := this.mapNode.Evaluate(state)
-	if !mlrval.IsMap() {
-		// TODO: more
-		return errors.New("Miller: looped-over item is not a map.")
-	}
-	mapval := mlrval.GetMap()
-
-	state.stack.PushStackFrame()
-	defer state.stack.PopStackFrame()
-	for pe := mapval.Head; pe != nil; pe = pe.Next {
-		mapkey := types.MlrvalFromString(*pe.Key)
-
-		// Note: The statement-block has its own push/pop for its localvars.
-		// Meanwhile we need to restrict scope of the bindvar to the for-loop.
-		//
-		// So we have:
-		//
-		//   mlr put '
-		//     x = 1;           <--- frame #1 main
-		//     for (k in $*) {  <--- frame #2 for for-loop bindvars (right here)
-		//       x = 2          <--- frame #3 for for-loop locals
-		//     }
-		//     x = 3;           <--- back in frame #1 main
-		//   '
-		//
-
-		state.stack.BindVariable(this.keyVariableName, &mapkey)
-		//state.stack.Dump()
-		err := this.statementBlockNode.Execute(state)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 // ================================================================
 type ForLoopKeyValueNode struct {
 	keyVariableName    string
 	valueVariableName  string
-	mapNode            IEvaluable
+	indexableNode      IEvaluable
 	statementBlockNode *StatementBlockNode
 }
 
 func NewForLoopKeyValueNode(
 	keyVariableName string,
 	valueVariableName string,
-	mapNode IEvaluable,
+	indexableNode IEvaluable,
 	statementBlockNode *StatementBlockNode,
 ) *ForLoopKeyValueNode {
 	return &ForLoopKeyValueNode{
 		keyVariableName,
 		valueVariableName,
-		mapNode,
+		indexableNode,
 		statementBlockNode,
 	}
 }
@@ -167,7 +110,7 @@ func BuildForLoopKeyValueNode(astNode *dsl.ASTNode) (*ForLoopKeyValueNode, error
 
 	keyVariableASTNode := astNode.Children[0]
 	valueVariableASTNode := astNode.Children[1]
-	mapASTNode := astNode.Children[2]
+	indexableASTNode := astNode.Children[2]
 	blockASTNode := astNode.Children[3]
 
 	lib.InternalCodingErrorIf(keyVariableASTNode.Type != dsl.NodeTypeLocalVariable)
@@ -180,7 +123,7 @@ func BuildForLoopKeyValueNode(astNode *dsl.ASTNode) (*ForLoopKeyValueNode, error
 
 	// TODO: error if loop-over node isn't Mappable (inasmuch as can be
 	// detected at CST-build time)
-	mapNode, err := BuildEvaluableNode(mapASTNode)
+	indexableNode, err := BuildEvaluableNode(indexableASTNode)
 	if err != nil {
 		return nil, err
 	}
@@ -193,47 +136,74 @@ func BuildForLoopKeyValueNode(astNode *dsl.ASTNode) (*ForLoopKeyValueNode, error
 	return NewForLoopKeyValueNode(
 		keyVariableName,
 		valueVariableName,
-		mapNode,
+		indexableNode,
 		statementBlockNode,
 	), nil
 }
 
 // ----------------------------------------------------------------
+// Note: The statement-block has its own push/pop for its localvars.
+// Meanwhile we need to restrict scope of the bindvar to the for-loop.
+//
+// So we have:
+//
+//   mlr put '
+//     x = 1;           <--- frame #1 main
+//     for (k in $*) {  <--- frame #2 for for-loop bindvars (right here)
+//       x = 2          <--- frame #3 for for-loop locals
+//     }
+//     x = 3;           <--- back in frame #1 main
+//   '
+//
+
 func (this *ForLoopKeyValueNode) Execute(state *State) error {
+	mlrval := this.indexableNode.Evaluate(state)
 
-	mlrval := this.mapNode.Evaluate(state)
-	if !mlrval.IsMap() {
-		// TODO: more
-		return errors.New("Miller: looped-over item is not a map.")
-	}
-	mapval := mlrval.GetMap()
+	if mlrval.IsMap() {
 
-	state.stack.PushStackFrame()
-	defer state.stack.PopStackFrame()
-	for pe := mapval.Head; pe != nil; pe = pe.Next {
-		mapkey := types.MlrvalFromString(*pe.Key)
+		mapval := mlrval.GetMap()
 
-		// Note: The statement-block has its own push/pop for its localvars.
-		// Meanwhile we need to restrict scope of the bindvar to the for-loop.
-		//
-		// So we have:
-		//
-		//   mlr put '
-		//     x = 1;           <--- frame #1 main
-		//     for (k in $*) {  <--- frame #2 for for-loop bindvars (right here)
-		//       x = 2          <--- frame #3 for for-loop locals
-		//     }
-		//     x = 3;           <--- back in frame #1 main
-		//   '
-		//
+		state.stack.PushStackFrame()
+		defer state.stack.PopStackFrame()
+		for pe := mapval.Head; pe != nil; pe = pe.Next {
+			mapkey := types.MlrvalFromString(*pe.Key)
 
-		state.stack.BindVariable(this.keyVariableName, &mapkey)
-		state.stack.BindVariable(this.valueVariableName, pe.Value)
-		//state.stack.Dump()
-		err := this.statementBlockNode.Execute(state)
-		if err != nil {
-			return err
+			state.stack.BindVariable(this.keyVariableName, &mapkey)
+			if this.valueVariableName != "" { // 'for (k in ...)' not 'for (k,v in ...)'
+				state.stack.BindVariable(this.valueVariableName, pe.Value)
+			}
+			//state.stack.Dump()
+			err := this.statementBlockNode.Execute(state)
+			if err != nil {
+				return err
+			}
 		}
+
+	} else if mlrval.IsArray() {
+
+		arrayval := mlrval.GetArray()
+
+		// Miller user-space array indices ("mindex") are 1-up. Internal Go
+		// storage ("zindex") is 0-up.
+		state.stack.PushStackFrame()
+		defer state.stack.PopStackFrame()
+		for zindex, element := range arrayval {
+			mindex := types.MlrvalFromInt64(int64(zindex + 1))
+
+			state.stack.BindVariable(this.keyVariableName, &mindex)
+			if this.valueVariableName != "" { // 'for (k in ...)' not 'for (k,v in ...)'
+				state.stack.BindVariable(this.valueVariableName, &element)
+			}
+			// state.stack.Dump()
+			err := this.statementBlockNode.Execute(state)
+			if err != nil {
+				return err
+			}
+		}
+
+	} else {
+		// TODO: give more context
+		return errors.New("Miller: looped-over item is not a map.")
 	}
 
 	return nil
