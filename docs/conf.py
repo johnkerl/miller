2d738e22babdd9e11935f955207e0fbb4a2122ca
John Kerl <kerl.john.r@gmail.com>
2020-09-27 10:22:32 -0400

    contents.rst -> index.rst again ... trying to fix 404

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,8 @@ release = '5.9.1'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
