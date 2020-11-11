9c8eb26687f71e600b79975aedc1308790f5c78e
John Kerl <kerl.john.r@gmail.com>
2020-11-11 14:14:20 -0500

    parser-experiments iterate

--- go/parser-experiments/main.go
+++ go/parser-experiments/main.go
@@ -9,45 +9,54 @@ import (
 )
 
 func parseOne(input string) {
-	fmt.Println("----------------------------------------------------------------")
-	fmt.Printf("Parsing \"%s\"\n", input)
 	theLexer := lexer.NewLexer([]byte(input))
 	theParser := parser.NewParser()
 	_, err := theParser.Parse(theLexer)
+
+	green := "\033[32;01m"
+	red := "\033[31;01m"
+	textdefault := "\033[0m"
+
 	if err != nil {
 		//fmt.Println(err)
-		fmt.Println("Fail")
+		fmt.Printf("%sFail%s %s\n", red, textdefault, input)
 	} else {
-		fmt.Println("OK")
+		fmt.Printf("%sOK%s   %s\n", green, textdefault, input)
 	}
 }
 
 func main() {
 	if len(os.Args) == 1 {
 		inputs := []string{
+			// Expect pass
 			"",
 			";",
+			";;",
 			"x",
 			"x;x",
 			"x;",
+			"x;;",
 			";x",
+			";;x",
 			"x ; {}",
 			"{} ; x",
 			"{} x",
+			"{ x }",
+			"{ x; x }",
+			"x; { x; x }",
+			"{ x; x } x",
+			"{ x; x } ; x",
+
+			// Expect fail
+			"x x",
+			"x {}",
 		}
-		for i, input := range inputs {
-			if i > 0 {
-				fmt.Println()
-			}
+		for _, input := range inputs {
 			parseOne(input)
 		}
 	} else {
-		for i, arg := range os.Args[1:] {
-			if i > 0 {
-				fmt.Println()
-			}
+		for _, arg := range os.Args[1:] {
 			parseOne(arg)
 		}
 	}
-	fmt.Println("----------------------------------------------------------------")
 }
