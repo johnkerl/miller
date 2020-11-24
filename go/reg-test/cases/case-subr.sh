5f90e92633af8506b4ae1a0ebcfa747b49ad2652
John Kerl <kerl.john.r@gmail.com>
2020-11-24 13:51:18 -0500

    merge case case-c-dsl-user-defined-subroutines.sh into case-subr.sh

--- go/reg-test/cases/case-subr.sh
+++ go/reg-test/cases/case-subr.sh
@@ -28,3 +28,17 @@ run_mlr --from $indir/2.dkvp put -v -q '
   print s(NR);
   call s("WORLD");
 '
+
+# Test recursion
+run_mlr --opprint --from $indir/abixy head -n 5 then put '
+    subr s(n) {
+        print "n = " . n;
+        if (is_numeric(n)) {
+            if (n > 0) {
+                call s(n-1)
+            }
+        }
+    }
+    print "";
+    call s(NR)
+'
