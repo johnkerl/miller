7fceba4fb800df9cd8ecf69b1a029479bfa65f27
John Kerl <kerl.john.r@gmail.com>
2020-11-25 23:46:25 -0500

    typo-fix

--- go/reg-test/cases/case-env.sh
+++ go/reg-test/cases/case-env.sh
@@ -6,7 +6,7 @@ run_mlr -n put -v '$ENV["FOO"] = "bar"'
 run_mlr -n put -v '$ENV["FOO"][2] = "bar"'
 
 export FOO=BAR
-run_mlr --from s --opprint head -n 2 then put '$z = ENV["FOO"]'
-run_mlr --from s --opprint head -n 2 then put '$z = ENV["FOO"][1]'
-run_mlr --from s --opprint head -n 2 then put 'ENV["FOO"] = "bar"'
-mlr_expect_fail --from s --opprint head -n 2 then put 'ENV["FOO"][2] = "bar"'
+run_mlr --from $indir/s.dkvp --opprint head -n 2 then put '$z = ENV["FOO"]'
+run_mlr --from $indir/s.dkvp --opprint head -n 2 then put '$z = ENV["FOO"][1]'
+run_mlr --from $indir/s.dkvp --opprint head -n 2 then put 'ENV["FOO"] = "bar"'
+mlr_expect_fail --from $indir/s.dkvp --opprint head -n 2 then put 'ENV["FOO"][2] = "bar"'
