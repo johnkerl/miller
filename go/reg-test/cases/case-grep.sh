386383e641978eadf49d6935d7b8dbdd82878336
John Kerl <kerl.john.r@gmail.com>
2020-11-29 10:18:55 -0500

    merge redundant case-files

--- go/reg-test/cases/case-grep.sh
+++ go/reg-test/cases/case-grep.sh
@@ -1,2 +1,5 @@
-run_mlr grep    pan $indir/abixy-het
-run_mlr grep -v pan $indir/abixy-het
+run_mlr --opprint --from $indir/s.dkvp grep pan
+run_mlr --opprint --from $indir/s.dkvp grep -v pan
+run_mlr --opprint --from $indir/s.dkvp grep PAN
+run_mlr --opprint --from $indir/s.dkvp grep -i PAN
+run_mlr --opprint --from $indir/s.dkvp grep -i -v PAN
