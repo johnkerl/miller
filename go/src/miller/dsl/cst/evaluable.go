ad83618ebd83e4fc2284d9dd01c0659931a28ae1
John Kerl <kerl.john.r@gmail.com>
2021-01-19 00:00:08 -0500

    fix bug w/ indirect oosvar @ RHS

--- go/src/miller/dsl/cst/evaluable.go
+++ go/src/miller/dsl/cst/evaluable.go
@@ -51,6 +51,8 @@ func (this *RootNode) BuildEvaluableNode(astNode *dsl.ASTNode) (IEvaluable, erro
 
 	case dsl.NodeTypeIndirectFieldValue: // $[...]
 		return this.BuildIndirectFieldValueNode(astNode)
+	case dsl.NodeTypeIndirectOosvarValue: // $[...]
+		return this.BuildIndirectOosvarValueNode(astNode)
 
 	case dsl.NodeTypeEnvironmentVariable: // ENV["NAME"]
 		return this.BuildEnvironmentVariableNode(astNode)
@@ -103,6 +105,39 @@ func (this *IndirectFieldValueNode) Evaluate(state *State) types.Mlrval { // xxx
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	if value == nil {
+		return types.MlrvalFromAbsent()
+	}
+	return *value
+}
+
+// ----------------------------------------------------------------
+type IndirectOosvarValueNode struct {
+	oosvarNameEvaluable IEvaluable
+}
+
+func (this *RootNode) BuildIndirectOosvarValueNode(
+	astNode *dsl.ASTNode,
+) (*IndirectOosvarValueNode, error) {
+	lib.InternalCodingErrorIf(astNode.Type != dsl.NodeTypeIndirectOosvarValue)
+	lib.InternalCodingErrorIf(astNode.Children == nil)
+	lib.InternalCodingErrorIf(len(astNode.Children) != 1)
+	oosvarNameEvaluable, err := this.BuildEvaluableNode(astNode.Children[0])
+	if err != nil {
+		return nil, err
+	}
+	return &IndirectOosvarValueNode{
+		oosvarNameEvaluable: oosvarNameEvaluable,
+	}, nil
+}
+
+func (this *IndirectOosvarValueNode) Evaluate(state *State) types.Mlrval { // xxx err
+	oosvarName := this.oosvarNameEvaluable.Evaluate(state)
+	if oosvarName.IsAbsent() {
+		return types.MlrvalFromAbsent()
+	}
+
+	value := state.Oosvars.Get(oosvarName.String())
 	if value == nil {
 		return types.MlrvalFromAbsent()
 	}
