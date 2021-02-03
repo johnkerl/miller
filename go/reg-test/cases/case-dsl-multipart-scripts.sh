0ed9f11fbc60e2f2e52e5903786083080cb3f0f5
John Kerl <john.kerl@auth0.com>
2021-02-03 03:52:02 +0000

    separate ingest-AST and resolve passes for the CST-builder

--- go/reg-test/cases/case-dsl-multipart-scripts.sh
+++ go/reg-test/cases/case-dsl-multipart-scripts.sh
@@ -8,5 +8,4 @@ run_mlr --opprint --from $indir/abixy filter -e 'NR == 7'
 
 run_mlr --opprint --from $indir/abixy put -e 'print "PRE";' -f $indir/put-script-piece-1 -f $indir/put-script-piece-2 -f $indir/put-script-piece-3 -e 'print "POST"'
 
-run_mlr --opprint --from $indir/abixy filter -f $indir/filter-script-piece-1
 run_mlr --opprint --from $indir/abixy filter -f $indir/filter-script-piece-1 -f $indir/filter-script-piece-2
--- go/src/miller/dsl/cst/root.go
+++ go/src/miller/dsl/cst/root.go
@@ -46,7 +46,11 @@ func (this *RootNode) WithRedefinableUDFS() *RootNode {
 }
 
 // ----------------------------------------------------------------
-func (this *RootNode) Build(
+// If the user has multiple put -f / put -e pieces, we can AST-parse each
+// separately and build them. However we cannot resolve UDF/UDS references
+// until after they're all ingested -- e.g. first piece calls a function which
+// the second defines, or mutual recursion across pieces, etc.
+func (this *RootNode) IngestAST(
 	ast *dsl.AST,
 	isFilter bool, // false for 'mlr put', true for 'mlr filter'
 ) error {
@@ -67,7 +71,12 @@ func (this *RootNode) Build(
 		return err
 	}
 
-	err = this.resolveFunctionCallsites()
+	return nil
+}
+
+func (this *RootNode) Resolve() error {
+
+	err := this.resolveFunctionCallsites()
 	if err != nil {
 		return err
 	}
