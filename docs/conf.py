14052346fa7d307aec66bd586f4041c53d002310
John Kerl <kerl.john.r@gmail.com>
2021-03-23 23:22:07 -0400

    update docs/conf.py for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, John Kerl'
 author = 'John Kerl'
 
 # The full version, including alpha/beta/rc tags
-release = '5.10.1'
+release = '5.10.2'
 
 # -- General configuration ---------------------------------------------------
 master_doc = 'index'
