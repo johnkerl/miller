6b42212c103eeceeb37cccc81bd669b4beabeeaa
John Kerl <kerl.john.r@gmail.com>
2021-03-28 10:31:21 -0400

    iterating

---
+++ experiments/cli-parser/msys2in.sh
@@ -0,0 +1,12 @@
+/home/kerlj/git/miller/experiments/cli-parser/cliparse.exe '$c=$a."bbb"'
+# <<C:\tools\msys64\home\kerlj\git\miller\experiments\cli-parser\cliparse.exe "$c=$a.\"bbb\"">>
+#
+# args[0] <<C:\tools\msys64\home\kerlj\git\miller\experiments\cli-parser\cliparse.exe>>
+# args[1] <<$c=$a."bbb">>
+
+$ /c/users/kerlj/git/miller/experiments/cli-parser/cliparse.exe '$c=$a."bbb"'
+# <<C:\users\kerlj\git\miller\experiments\cli-parser\cliparse.exe "$c=$a.\"bbb\"">>
+#
+# args[0] <<C:\users\kerlj\git\miller\experiments\cli-parser\cliparse.exe>>
+# args[1] <<$c=$a."bbb">>
+
