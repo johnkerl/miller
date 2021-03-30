b1d625f4279434aa601efa267915097fb2702e83
John Kerl <kerl.john.r@gmail.com>
2021-03-30 00:15:33 -0400

    first mlr-exec reg-test trials

--- go/regression/regression_test.go
+++ go/regression/regression_test.go
@@ -7,9 +7,38 @@ package regression
 import (
 	"os"
 	"testing"
+
+	"miller/regression/support"
 )
 
 func TestWorkingDirectory(t *testing.T) {
-         wd, _ := os.Getwd()
-         t.Log(wd)
+	wd, _ := os.Getwd()
+	t.Log("pwd is:", wd)
+}
+
+func TestFoo(t *testing.T) {
+	stdout, stderr, exitCode, err := support.RunMillerCommand(os.Args[0], "cat testdata/abixy")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// TODO: really want multiple verbosity levels ...
+	t.Log("stdout <<", stdout, ">>")
+	t.Log("stderr <<", stderr, ">>")
+	t.Log("exitCode", exitCode)
+	if exitCode != 0 {
+		t.Fatal()
+	}
+}
+
+func TestBar(t *testing.T) {
+	stdout, stderr, exitCode, err := support.RunMillerCommand(os.Args[0], "cxt testdata/abixy")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("stdout <<", stdout, ">>")
+	t.Log("stderr <<", stderr, ">>")
+	t.Log("exitCode", exitCode)
+	if exitCode != 1 {
+		t.Fatal()
+	}
 }
---
+++ go/regression/support/support.go
@@ -0,0 +1,52 @@
+package support
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+
+	shellquote "github.com/kballard/go-shellquote"
+)
+
+func RunMillerCommand(
+	millerExe string,
+	argsString string,
+) (
+	stdout string,
+	stderr string,
+	exitCode int,
+	executionError error, // TODO: comment: failure to even start the process
+) {
+
+	// xxx
+	millerExe = "../mlr"
+
+	fmt.Printf("%s %s\n", millerExe, argsString)
+	argsArray, err := shellquote.Split(argsString)
+	if err != nil {
+		return "", "", -1, err
+	}
+
+	cmd := exec.Command(millerExe, argsArray...)
+
+	var stdoutBuffer bytes.Buffer
+	var stderrBuffer bytes.Buffer
+
+	cmd.Stdout = &stdoutBuffer
+	cmd.Stderr = &stderrBuffer
+
+	err = cmd.Run()
+
+	exitCode = 0
+	stdout = stdoutBuffer.String()
+	stderr = stderrBuffer.String()
+
+	if err != nil {
+		exitError, ok := err.(*exec.ExitError)
+		if ok {
+			exitCode = exitError.ExitCode()
+		}
+	}
+
+	return stdout, stderr, exitCode, nil
+}
