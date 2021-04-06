06c754ebdf9bbe9d38a8c8cf67bf9b9ec8074f97
John Kerl <kerl.john.r@gmail.com>
2021-04-06 01:46:45 -0400

    reg-test/cases/case-dsl-type-reflect.sh

---
+++ go/reg-test/cases/dsl-type-reflect/0001.cmd
@@ -0,0 +1,1 @@
+mlr --j2p --from reg-test/input/typecast.json put -f ./reg-test/cases/dsl-type-reflect/0001.mlr then reorder -f t,a
---
+++ go/reg-test/cases/dsl-type-reflect/0003.cmd
@@ -0,0 +1,1 @@
+mlr --idkvp --opprint --from reg-test/input/s.dkvp put -f ./reg-test/cases/dsl-type-reflect/0003.mlr
