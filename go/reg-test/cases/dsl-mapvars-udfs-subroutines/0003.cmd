aa30197c5e2ee389a1c139335078add69ae6520b
John Kerl <kerl.john.r@gmail.com>
2021-04-06 01:10:53 -0400

    dsl-mapvars-udfs-subroutines

---
+++ go/reg-test/cases/dsl-mapvars-udfs-subroutines/0003.cmd
@@ -0,0 +1,1 @@
+mlr --from reg-test/input/abixy-het put    'func f(x) {return {"a":x,"b":x**2}}; map o = f($x); $* = o'
---
+++ go/reg-test/cases/dsl-mapvars-udfs-subroutines/0004.cmd
@@ -0,0 +1,1 @@
+mlr --from reg-test/input/abixy-het put -q 'func f(x) {return x**2}; var z = f($x); dump z'
