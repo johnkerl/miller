d6f46a94d000586b034d8ee14aa6a903d8082706
John Kerl <kerl.john.r@gmail.com>
2021-04-06 10:11:40 -0400

    reg-test/cases/dsl-put-multiple-dash-e

--- go/reg-test/cases/case-port-me.sh
+++ go/reg-test/cases/case-port-me.sh
@@ -1,3 +1,4 @@
+# TODO: pending .env
 
 # mention LHS value on first record should result in ZYX for process creation
 export indir; run_mlr --from $indir/abixy put -q 'ENV["ZYX"]="CBA".NR; print | ENV["indir"]."/env-assign.sh" , "a is " . $a'
