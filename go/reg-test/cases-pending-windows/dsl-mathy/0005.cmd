6048a55b123d07c25574823e79fbcdd10c76c77f
John Kerl <kerl.john.r@gmail.com>
2021-04-07 19:22:10 -0400

    more

--- go/reg-test/cases-pending-windows/dsl-mathy/0005.cmd
+++
@@ -1,1 +0,0 @@
-mlr --from reg-test/input/ten.dkvp --opprint put -f reg-test/cases-pending-windows/dsl-mathy/0005.mlr
---
+++ go/reg-test/cases/dsl-mathy/0002.cmd
@@ -0,0 +1,1 @@
+mlr --from reg-test/input/s.dkvp --opprint put -f reg-test/cases/dsl-mathy/0002.mlr
