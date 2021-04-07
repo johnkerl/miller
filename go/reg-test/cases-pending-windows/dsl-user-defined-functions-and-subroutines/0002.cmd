d8866dcc5e1730cdb29c2207e5b7c77b57dfb1ef
John Kerl <kerl.john.r@gmail.com>
2021-04-07 19:32:47 -0400

    more

--- go/reg-test/cases-pending-windows/dsl-user-defined-functions-and-subroutines/0002.cmd
+++
@@ -1,1 +0,0 @@
-mlr --opprint --from reg-test/input/abixy put -f ./reg-test/cases-pending-windows/dsl-user-defined-functions-and-subroutines/0002.mlr
--- go/reg-test/cases-pending-windows/dsl-user-defined-functions-and-subroutines/0016.cmd
+++
@@ -1,1 +0,0 @@
-mlr --from reg-test/input/abixy --opprint put -f reg-test/cases-pending-windows/dsl-user-defined-functions-and-subroutines/0016.mlr
