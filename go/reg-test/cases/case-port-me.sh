9e3a768f4e90923d42459d7727b9febba7456c5c
John Kerl <kerl.john.r@gmail.com>
2021-04-06 23:43:44 -0400

    more

--- go/reg-test/cases/case-port-me.sh
+++
@@ -1,5 +0,0 @@
-# TODO: pending .env
-# TODO: pending multifile
-
-# mention LHS value on first record should result in ZYX for process creation
-export indir; run_mlr --from $indir/abixy put -q 'ENV["ZYX"]="CBA".NR; print | ENV["indir"]."/env-assign.sh" , "a is " . $a'
