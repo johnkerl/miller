a88ce834325bcdb5025584cf3c2af51ab2a707a8
John Kerl <kerl.john.r@gmail.com>
2021-04-29 09:56:25 -0400

    fill-down --all

--- go/src/transformers/fill-down.go
+++ go/src/transformers/fill-down.go
@@ -33,6 +33,7 @@ func transformerFillDownUsage(
 	fmt.Fprintln(o, "With -a, a field is 'missing' only if it is absent.")
 	fmt.Fprintln(o, "")
 	fmt.Fprintln(o, "Options:")
+	fmt.Fprintln(o, " --all Operate on all fields in the input.\n")
 	fmt.Fprintln(o, " -a|--only-if-absent If a given record has a missing value for a given field,")
 	fmt.Fprintln(o, "     fill that from the corresponding value from a previous record, if any.")
 	fmt.Fprintln(o, "     By default, a 'missing' field either is absent, or has the empty-string value.")
@@ -59,6 +60,7 @@ func transformerFillDownParseCLI(
 	argi++
 
 	var fillDownFieldNames []string = nil
+	doAll := false
 	onlyIfAbsent := false
 
 	for argi < argc /* variable increment: 1 or 2 depending on flag */ {
@@ -74,6 +76,9 @@ func transformerFillDownParseCLI(
 		} else if opt == "-f" {
 			fillDownFieldNames = cliutil.VerbGetStringArrayArgOrDie(verb, opt, args, &argi, argc)
 
+		} else if opt == "--all" {
+			doAll = true
+
 		} else if opt == "-a" {
 			onlyIfAbsent = true
 
@@ -87,6 +92,7 @@ func transformerFillDownParseCLI(
 
 	transformer, _ := NewTransformerFillDown(
 		fillDownFieldNames,
+		doAll,
 		onlyIfAbsent,
 	)
 
@@ -98,14 +104,18 @@ func transformerFillDownParseCLI(
 type TransformerFillDown struct {
 	// input
 	fillDownFieldNames []string
+	doAll              bool
 	onlyIfAbsent       bool
 
 	// state
 	lastNonNullValues map[string]*types.Mlrval
+
+	recordTransformerFunc transforming.RecordTransformerFunc
 }
 
 func NewTransformerFillDown(
 	fillDownFieldNames []string,
+	doAll bool,
 	onlyIfAbsent bool,
 ) (*TransformerFillDown, error) {
 	this := &TransformerFillDown{
@@ -113,6 +123,13 @@ func NewTransformerFillDown(
 		onlyIfAbsent:       onlyIfAbsent,
 		lastNonNullValues:  make(map[string]*types.Mlrval),
 	}
+
+	if doAll {
+		this.recordTransformerFunc = this.transformAll
+	} else {
+		this.recordTransformerFunc = this.transformSpecified
+	}
+
 	return this, nil
 }
 
@@ -121,10 +138,55 @@ func (this *TransformerFillDown) Transform(
 	inrecAndContext *types.RecordAndContext,
 	outputChannel chan<- *types.RecordAndContext,
 ) {
+	this.recordTransformerFunc(inrecAndContext, outputChannel)
+}
+
+// ----------------------------------------------------------------
+func (this *TransformerFillDown) transformSpecified(
+	inrecAndContext *types.RecordAndContext,
+	outputChannel chan<- *types.RecordAndContext,
+) {
 	if !inrecAndContext.EndOfStream {
 		inrec := inrecAndContext.Record
 
 		for _, fillDownFieldName := range this.fillDownFieldNames {
+			present := false
+			value := inrec.Get(fillDownFieldName)
+			if this.onlyIfAbsent {
+				present = value != nil
+			} else {
+				present = value != nil && !value.IsEmpty()
+			}
+
+			if present {
+				// Remember it for a subsequent record lacking this field
+				this.lastNonNullValues[fillDownFieldName] = value.Copy()
+			} else {
+				// Reuse previously seen value, if any
+				prev, ok := this.lastNonNullValues[fillDownFieldName]
+				if ok {
+					inrec.PutCopy(fillDownFieldName, prev)
+				}
+			}
+		}
+
+		outputChannel <- inrecAndContext
+
+	} else {
+		outputChannel <- inrecAndContext // end-of-stream marker
+	}
+}
+
+// ----------------------------------------------------------------
+func (this *TransformerFillDown) transformAll(
+	inrecAndContext *types.RecordAndContext,
+	outputChannel chan<- *types.RecordAndContext,
+) {
+	if !inrecAndContext.EndOfStream {
+		inrec := inrecAndContext.Record
+
+		for pe := inrec.Head; pe != nil; pe = pe.Next {
+			fillDownFieldName := pe.Key
 			present := false
 			value := inrec.Get(fillDownFieldName)
 			if this.onlyIfAbsent {
