cd36fe550871f5386f9979df5a3e167e3c452fcb
John Kerl <kerl.john.r@gmail.com>
2021-05-12 23:41:36 -0400

    Support scanning null on JSON input

--- go/src/types/mlrval_json.go
+++ go/src/types/mlrval_json.go
@@ -134,7 +134,7 @@ func MlrvalDecodeFromJSON(decoder *json.Decoder) (
 	delimiter, isDelim := startToken.(json.Delim)
 	if !isDelim {
 		if startToken == nil {
-			return MLRVAL_VOID, false, nil
+			return MLRVAL_NULL, false, nil
 		}
 
 		sval, ok := startToken.(string)
