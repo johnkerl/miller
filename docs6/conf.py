17761849e178cb556f46d4a91bbba5d76abbd755
John Kerl <kerl.john.r@gmail.com>
2021-06-15 02:25:29 +0000

    Make docs more mobile-friendly (#572)

--- docs6/conf.py
+++ docs6/conf.py
@@ -47,9 +47,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'classic'
+#html_theme = 'classic'
 #html_theme = 'sphinxdoc'
 #html_theme = 'nature'
+html_theme = 'scrolls'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
