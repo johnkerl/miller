df8e29e9e834f7868dcde2af8fb980e4ff6a0baf
John Kerl <kerl.john.r@gmail.com>
2021-10-16 13:06:00 -0400

    Fix emit-flatten when main & emit output file formats differ (#702)
    * xvright
    
    * Left/right trim on --repifs
    
    * Fix emit-flatten when main & emit output file formats differ

--- go/src/auxents/repl/entry.go
+++ go/src/auxents/repl/entry.go
@@ -149,7 +149,7 @@ func ReplMain(args []string) int {
 
 	// --auto-flatten is on by default. But if input and output formats are both JSON,
 	// then we don't need to actually do anything. See also mlrcli_parse.go.
-	options.WriterOptions.AutoFlatten = cli.DecideFinalFlatten(&options)
+	options.WriterOptions.AutoFlatten = cli.DecideFinalFlatten(&options.WriterOptions)
 	options.WriterOptions.AutoUnflatten = cli.DecideFinalUnflatten(&options)
 
 	repl, err := NewRepl(
