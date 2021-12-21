dc5afa562575e54e2126fe1a37130e0b6bf331d5
John Kerl <kerl.john.r@gmail.com>
2021-12-21 09:39:30 -0500

    Copy-reduction performance in Mlrval.String()

--- scripts/chain-cmps.sh
+++ scripts/chain-cmps.sh
@@ -1,41 +1,9 @@
-echo; for m in mlr5 ~/tmp/miller/mlr "./mlr -S" mlr; do
-  justtime $m --csv --from ~/tmp/big.csv \
-    check \
-  | md5sum
-done
+mlrs="mlr5 ~/tmp/miller/mlr ./mlr"
 
-echo; for m in mlr5 ~/tmp/miller/mlr "./mlr -S" mlr; do
-  justtime $m --csv --from ~/tmp/big.csv \
-    cat \
-  | md5sum;
-done
-
-echo; for m in mlr5 ~/tmp/miller/mlr "./mlr -S" mlr; do
-  justtime $m --csv --from ~/tmp/big.csv \
-    head \
-  | md5sum;
-done
-
-echo; for m in mlr5 ~/tmp/miller/mlr "./mlr -S" mlr; do
-  justtime $m --csv --from ~/tmp/big.csv \
-    tail \
-  | md5sum;
-done
-
-echo; for m in mlr5 ~/tmp/miller/mlr "./mlr -S" mlr; do
-  justtime $m --csv --from ~/tmp/big.csv \
-    tac \
-  | md5sum;
-done
-
-echo; for m in mlr5 ~/tmp/miller/mlr "./mlr -S" mlr; do
-  justtime $m --csv --from ~/tmp/big.csv \
-    sort -f shape \
-  | md5sum;
-done
-
-echo; for m in mlr5 ~/tmp/miller/mlr "./mlr -S" mlr; do
-  justtime $m --csv --from ~/tmp/big.csv \
-    sort -n quantity \
-  | md5sum;
-done
+echo; for mlr in $mlrs; do justtime $mlr --csv --from ~/tmp/big.csv check |  md5sum;  done
+echo; for mlr in $mlrs; do justtime $mlr --csv --from ~/tmp/big.csv cat   |  md5sum;  done
+echo; for mlr in $mlrs; do justtime $mlr --csv --from ~/tmp/big.csv head  |  md5sum;  done
+echo; for mlr in $mlrs; do justtime $mlr --csv --from ~/tmp/big.csv tail  |  md5sum;  done
+echo; for mlr in $mlrs; do justtime $mlr --csv --from ~/tmp/big.csv tac   |  md5sum;  done
+echo; for mlr in $mlrs; do justtime $mlr --csv --from ~/tmp/big.csv sort -f shape    | md5sum; done
+echo; for mlr in $mlrs; do justtime $mlr --csv --from ~/tmp/big.csv sort -n quantity | md5sum; done
