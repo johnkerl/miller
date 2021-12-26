115d99999e559d1d9c9bfd1b5ed1c44fb0df76fc
John Kerl <kerl.john.r@gmail.com>
2021-12-25 23:45:40 -0500

    neaten perf-scripts

--- scripts/chain-cmps.sh
+++ scripts/chain-cmps.sh
@@ -1,13 +1,13 @@
-mlrs="mlr5 ~/tmp/miller/mlr ./mlr"
-reps="1"
+#mlrs="mlr5 ~/tmp/miller/mlr ./mlr"
+#reps="1"
 
-#mlrs="mlr5 ./mlr"
-#reps="1 2 3"
+mlrs="mlr5 ./mlr"
+reps="1 2 3"
 
-echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv check |  md5sum;  done; done
-echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv cat   |  md5sum;  done; done
-echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv head  |  md5sum;  done; done
-echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv tail  |  md5sum;  done; done
-echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv tac   |  md5sum;  done; done
-echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv sort -f shape    | md5sum; done; done
-echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv sort -n quantity | md5sum; done; done
+echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv check > /dev/null;  done; done
+echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv cat   > /dev/null;  done; done
+echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv head  > /dev/null;  done; done
+echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv tail  > /dev/null;  done; done
+echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv tac   > /dev/null;  done; done
+echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv sort -f shape    > /dev/null; done; done
+echo; for mlr in $mlrs; do for k in $reps; do justtime $mlr --csv --from ~/tmp/big.csv sort -n quantity > /dev/null; done; done
