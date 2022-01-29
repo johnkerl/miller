930162c200e6968df298c7e6e4ccbd6e48fdc73c
John Kerl <kerl.john.r@gmail.com>
2022-01-29 13:20:08 -0500

    add fixed-length option to ngrams

--- docs/src/ngrams/ngrams.sh
+++ docs/src/ngrams/ngrams.sh
@@ -3,6 +3,20 @@
 # ================================================================
 # See for context
 # https://miller.readthedocs.io/en/latest/randomizing-examples/#randomly-generating-jabberwocky-words
+#
+#
+# Example with fixed-length output:
+# $ shuf -n 10000 ~/misc/words ./ngrams/ngrams.sh -n 3  -l 4 | Jj | Jj | sed 's/ /-/g'
+# devo-bils-obug-auna
+# pred-main-ened-ored
+# tric-sual-afty-pric
+# blas-lunt-buti-roma
+# rect-knos-vish-snes
+# fles-ches-oses-plac
+# gall-cods-swer-flic
+# uner-nals-hyde-sins
+# culs-bler-dist-lefs
+# aval-cold-ders-pers
 # ================================================================
 
 # ----------------------------------------------------------------
@@ -18,6 +32,7 @@ usage() {
   echo "Options:" 1>&2
   echo "-n {n} The n for n-grams; default $default_n." 1>&2
   echo "-o {o} Number of words to produce; default $default_ocount." 1>&2
+  echo "-l {l} Only make words of length l. Default: sample from input-length distribution." 1>&2
   echo "-v     Verbose processing; default off." 1>&2
   echo "If no wordlists are provided, stdin i=s read." 1>&2
   exit 1
@@ -27,13 +42,14 @@ usage() {
 n=$default_n
 ocount=$default_ocount
 verbose=$default_verbose
-wordlist=$default_wordlist
+olen=0
 
-while getopts n:o:vh? f
+while getopts n:o:l:vh? f
 do
   case $f in
     n)  n="$OPTARG";      continue;;
     o)  ocount="$OPTARG"; continue;;
+    l)  olen="$OPTARG";   continue;;
     v)  verbose="true";   continue;;
     h)  usage;;
     \?) usage;;
@@ -48,6 +64,6 @@ fi
 wordlist="$@"
 
 mlr --nidx put -q \
-  -s n=$n -s ocount=$ocount -s verbose=$verbose \
+  -s n=$n -s ocount=$ocount -s olen=$olen -s verbose=$verbose \
   -f $ourdir/ngfuncs.mlr -f $ourdir/ngrams.mlr \
   $wordlist
