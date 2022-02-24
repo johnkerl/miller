42628b83d3cd300646c2f53153e1ebfe7f940859
John Kerl <kerl.john.r@gmail.com>
2022-02-24 09:56:08 -0500

    Typofix in unformat/unformatx on-line help

--- internal/pkg/dsl/cst/builtin_function_manager.go
+++ internal/pkg/dsl/cst/builtin_function_manager.go
@@ -385,7 +385,7 @@ used within subsequent DSL statements. See also "Regular expressions" at ` + lib
 		{
 			name:       ".",
 			class:      FUNC_CLASS_STRING,
-			help:       `String concatenation.`,
+			help:       `String concatenation. Non-strings are coerced, so you can do '"ax".98' etc.`,
 			binaryFunc: bifs.BIF_dot,
 		},
 
@@ -569,8 +569,8 @@ each "{}" in the format string. Too-few arguments are treated as the empty strin
 			help: `Using first argument as format string, unpacks second argument into an array of matches,
 with type-inference. On non-match, returns error -- use is_error() to check.`,
 			examples: []string{
-				`unformat("{}:{}:{}",  "1:2:3") gives [1, 2, 3]".`,
-				`unformat("{}h{}m{}s", "3h47m22s") gives [3, 47, 22]".`,
+				`unformat("{}:{}:{}",  "1:2:3") gives [1, 2, 3].`,
+				`unformat("{}h{}m{}s", "3h47m22s") gives [3, 47, 22].`,
 				`is_error(unformat("{}h{}m{}s", "3:47:22")) gives true.`,
 			},
 			binaryFunc: bifs.BIF_unformat,
@@ -581,8 +581,8 @@ with type-inference. On non-match, returns error -- use is_error() to check.`,
 			class: FUNC_CLASS_STRING,
 			help:  `Same as unformat, but without type-inference.`,
 			examples: []string{
-				`unformatx("{}:{}:{}",  "1:2:3") gives ["1", "2", "3"]".`,
-				`unformatx("{}h{}m{}s", "3h47m22s") gives ["3", "47", "22"]".`,
+				`unformatx("{}:{}:{}",  "1:2:3") gives ["1", "2", "3"].`,
+				`unformatx("{}h{}m{}s", "3h47m22s") gives ["3", "47", "22"].`,
 				`is_error(unformatx("{}h{}m{}s", "3:47:22")) gives true.`,
 			},
 			binaryFunc: bifs.BIF_unformatx,
