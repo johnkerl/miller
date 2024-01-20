97b59edbd4a6ebfb389d5fd240f2deb78d651215
John Kerl <kerl.john.r@gmail.com>
2024-01-20 17:24:15 -0500

    Auto-unsparsify CSV

--- pkg/output/record_writer_csv.go
+++ pkg/output/record_writer_csv.go
@@ -3,7 +3,6 @@ package output
 import (
 	"bufio"
 	"fmt"
-	"strings"
 
 	csv "github.com/johnkerl/miller/pkg/go-csv"
 
@@ -12,15 +11,12 @@ import (
 )
 
 type RecordWriterCSV struct {
-	writerOptions *cli.TWriterOptions
-	ofs0          byte // Go's CSV library only lets its 'Comma' be a single character
-	csvWriter     *csv.Writer
-	// For reporting schema changes: we print a newline and the new header
-	lastJoinedHeader *string
-	// Only write one blank line for schema changes / blank input lines
-	justWroteEmptyLine bool
-	// For double-quote around all fields
-	quoteAll bool
+	writerOptions     *cli.TWriterOptions
+	ofs0              byte // Go's CSV library only lets its 'Comma' be a single character
+	csvWriter         *csv.Writer
+	needToPrintHeader bool
+	firstFieldCount   int64
+	quoteAll          bool // For double-quote around all fields
 }
 
 func NewRecordWriterCSV(writerOptions *cli.TWriterOptions) (*RecordWriterCSV, error) {
@@ -30,13 +26,14 @@ func NewRecordWriterCSV(writerOptions *cli.TWriterOptions) (*RecordWriterCSV, er
 	if writerOptions.ORS != "\n" && writerOptions.ORS != "\r\n" {
 		return nil, fmt.Errorf("for CSV, ORS cannot be altered")
 	}
-	return &RecordWriterCSV{
-		writerOptions:      writerOptions,
-		csvWriter:          nil, // will be set on first Write() wherein we have the output stream
-		lastJoinedHeader:   nil,
-		justWroteEmptyLine: false,
-		quoteAll:           writerOptions.CSVQuoteAll,
-	}, nil
+	writer := &RecordWriterCSV{
+		writerOptions:     writerOptions,
+		csvWriter:         nil, // will be set on first Write() wherein we have the output stream
+		needToPrintHeader: !writerOptions.HeaderlessOutput,
+		firstFieldCount:   -1,
+		quoteAll:          writerOptions.CSVQuoteAll,
+	}
+	return writer, nil
 }
 
 func (writer *RecordWriterCSV) Write(
@@ -54,46 +51,36 @@ func (writer *RecordWriterCSV) Write(
 		writer.csvWriter.Comma = rune(writer.writerOptions.OFS[0]) // xxx temp
 	}
 
-	if outrec.IsEmpty() {
-		if !writer.justWroteEmptyLine {
-			bufferedOutputStream.WriteString("\n")
-		}
-		joinedHeader := ""
-		writer.lastJoinedHeader = &joinedHeader
-		writer.justWroteEmptyLine = true
-		return
+	if writer.firstFieldCount == -1 {
+		writer.firstFieldCount = outrec.FieldCount
 	}
 
-	needToPrintHeader := false
-	joinedHeader := strings.Join(outrec.GetKeys(), ",")
-	if writer.lastJoinedHeader == nil || *writer.lastJoinedHeader != joinedHeader {
-		if writer.lastJoinedHeader != nil {
-			if !writer.justWroteEmptyLine {
-				bufferedOutputStream.WriteString("\n")
-			}
-			writer.justWroteEmptyLine = true
-		}
-		writer.lastJoinedHeader = &joinedHeader
-		needToPrintHeader = true
-	}
-
-	if needToPrintHeader && !writer.writerOptions.HeaderlessOutput {
+	if writer.needToPrintHeader {
 		fields := make([]string, outrec.FieldCount)
 		i := 0
 		for pe := outrec.Head; pe != nil; pe = pe.Next {
 			fields[i] = pe.Key
 			i++
 		}
-		//////writer.csvWriter.Write(fields)
 		writer.WriteCSVRecordMaybeColorized(fields, bufferedOutputStream, outputIsStdout, true, writer.quoteAll)
+		writer.needToPrintHeader = false
 	}
 
-	fields := make([]string, outrec.FieldCount)
-	i := 0
+	outputNF := outrec.FieldCount
+	if outputNF < writer.firstFieldCount {
+		outputNF = writer.firstFieldCount
+	}
+
+	fields := make([]string, outputNF)
+	var i int64 = 0
 	for pe := outrec.Head; pe != nil; pe = pe.Next {
 		fields[i] = pe.Value.String()
 		i++
 	}
+
+	for ; i < writer.firstFieldCount; i++ {
+		fields[i] = ""
+	}
+
 	writer.WriteCSVRecordMaybeColorized(fields, bufferedOutputStream, outputIsStdout, false, writer.quoteAll)
-	writer.justWroteEmptyLine = false
 }
