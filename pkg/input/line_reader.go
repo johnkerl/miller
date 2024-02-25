4b886c4e11c8061847c7a4c8723701893ddaef4f
John Kerl <kerl.john.r@gmail.com>
2024-02-24 22:15:19 -0500

    Switch to bufio.Reader, first pass

--- pkg/input/line_reader.go
+++ pkg/input/line_reader.go
@@ -7,6 +7,7 @@ import (
 	"bufio"
 	"container/list"
 	"io"
+	"strings"
 )
 
 type ILineReader interface {
@@ -15,69 +16,110 @@ type ILineReader interface {
 }
 
 type TLineReader struct {
-	scanner *bufio.Scanner
+	underlying *bufio.Reader
+	full_irs string
+	end_irs byte
+	staged string
+	eof bool
 }
 
 // NewLineReader handles reading lines which may be delimited by multi-line separators,
 // e.g. "\xe2\x90\x9e" for USV.
 func NewLineReader(handle io.Reader, irs string) *TLineReader {
-	scanner := bufio.NewScanner(handle)
+	underlying := bufio.NewReader(handle)
 
-	if irs == "\n" || irs == "\r\n" {
-		// Handled by default scanner.
-	} else {
-		irsbytes := []byte(irs)
-		irslen := len(irsbytes)
+// XXX TO DO: handle the splitter which bufio.Scanner has but bufio.Reader lacks
 
-		// Custom splitter
-		recordSplitter := func(
-			data []byte,
-			atEOF bool,
-		) (
-			advance int,
-			token []byte,
-			err error,
-		) {
-			datalen := len(data)
-			end := datalen - irslen
-			for i := 0; i <= end; i++ {
-				if data[i] == irsbytes[0] {
-					match := true
-					for j := 1; j < irslen; j++ {
-						if data[i+j] != irsbytes[j] {
-							match = false
-							break
-						}
-					}
-					if match {
-						return i + irslen, data[:i], nil
-					}
-				}
-			}
-			if !atEOF {
-				return 0, nil, nil
-			}
-			// There is one final token to be delivered, which may be the empty string.
-			// Returning bufio.ErrFinalToken here tells Scan there are no more tokens after this
-			// but does not trigger an error to be returned from Scan itself.
-			return 0, data, bufio.ErrFinalToken
-		}
+//	if irs == "\n" || irs == "\r\n" {
+//		// Handled by default scanner.
+//	} else {
+//		irsbytes := []byte(irs)
+//		irslen := len(irsbytes)
+//
+//		// Custom splitter
+//		recordSplitter := func(
+//			data []byte,
+//			atEOF bool,
+//		) (
+//			advance int,
+//			token []byte,
+//			err error,
+//		) {
+//			datalen := len(data)
+//			end := datalen - irslen
+//			for i := 0; i <= end; i++ {
+//				if data[i] == irsbytes[0] {
+//					match := true
+//					for j := 1; j < irslen; j++ {
+//						if data[i+j] != irsbytes[j] {
+//							match = false
+//							break
+//						}
+//					}
+//					if match {
+//						return i + irslen, data[:i], nil
+//					}
+//				}
+//			}
+//			if !atEOF {
+//				return 0, nil, nil
+//			}
+//			// There is one final token to be delivered, which may be the empty string.
+//			// Returning bufio.ErrFinalToken here tells Scan there are no more tokens after this
+//			// but does not trigger an error to be returned from Scan itself.
+//			return 0, data, bufio.ErrFinalToken
+//		}
+//
+//		underlying.Split(recordSplitter)
+//	}
 
-		scanner.Split(recordSplitter)
-	}
-
+	// XXX TEMP
 	return &TLineReader{
-		scanner: scanner,
+		underlying: underlying,
+		full_irs: irs,
+		end_irs: irs[0],
+		eof: false,
 	}
 }
 
+// XXX ERR RET
 func (r *TLineReader) Scan() bool {
-	return r.scanner.Scan()
+	line, err := r.underlying.ReadString(r.end_irs)
+	if err == nil {
+		r.staged = line
+		// XXX CHOMP
+		if strings.HasSuffix(line, r.full_irs) {
+			n := len(line)
+			// XXX CACHE THIS LEN
+			r.staged = line[:n-len(r.full_irs)]
+		}
+		r.eof = false
+		return true
+	} else if err == io.EOF {
+		r.staged = ""
+		r.eof = true
+		return false
+	} else {
+		panic("PORT ME")
+	}
 }
 
 func (r *TLineReader) Text() string {
-	return r.scanner.Text()
+	if r.eof {
+		panic("PORT ME TOO")
+	}
+	return r.staged
 }
+
+//		if err != nil {
+//			fmt.Printf("ERR %v\n", err)
+//			break
+//		}
+//		fmt.Printf("line len %d\n", len(line))
+//	}
+//	fmt.Println("AFTER LOOP")
+//}
+
 
 // TODO: comment copiously
 //
