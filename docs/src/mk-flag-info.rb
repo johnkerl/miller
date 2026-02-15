19d2efbf815a4f7f98d800b69d2a2c5871543af3
John Kerl <kerl.john.r@gmail.com>
2026-02-14 20:36:17 -0500

    more testing

--- docs/src/mk-flag-info.rb
+++ docs/src/mk-flag-info.rb
@@ -15,19 +15,22 @@ for section_name in section_names
   if section_name =~ /.*conversion.*keystroke-saver*/
     # The markdown in this section looks a lot better when hand-crafted (thanks Nikos!).
     puts <<EOF
-The letters `c`, `t`, `j`, `d`, `n`, `x`, `p`, and `m` refer to formats CSV, TSV, DKVP, NIDX, JSON, XTAB,
-PPRINT, and markdown, respectively. Note that markdown format is available for
+The letters `c`, `t`, `j`, `l`, `d`, `n`, `x`, `p`, `m`, and `y` refer to formats CSV, TSV, JSON, JSON Lines,
+DKVP, NIDX, XTAB, PPRINT, markdown, and YAML, respectively. Note that markdown format is available for
 output only.
 
-| In \ out   | **CSV** | **TSV** | **JSON** | **DKVP** | **NIDX** | **XTAB** | **PPRINT** | **Markdown** |
-|------------|---------|---------|----------|----------|----------|----------|------------|--------------|
-| **CSV**    |         | `--c2t` | `--c2j`  | `--c2d`  | `--c2n`  | `--c2x`  | `--c2p`    | `--c2m`      |
-| **TSV**    | `--t2c` |         | `--t2j`  | `--t2d`  | `--t2n`  | `--t2x`  | `--t2p`    | `--t2m`      |
-| **JSON**   | `--j2c` | `--j2t` |          | `--j2d`  | `--j2n`  | `--j2x`  | `--j2p`    | `--j2m`      |
-| **DKVP**   | `--d2c` | `--d2t` | `--d2j`  |          | `--d2n`  | `--d2x`  | `--d2p`    | `--d2m`      |
-| **NIDX**   | `--n2c` | `--n2t` | `--n2j`  | `--n2d`  |          | `--n2x`  | `--n2p`    | `--n2m`      |
-| **XTAB**   | `--x2c` | `--x2t` | `--x2j`  | `--x2d`  | `--x2n`  |          | `--x2p`    | `--x2m`      |
-| **PPRINT** | `--p2c` | `--p2t` | `--p2j`  | `--p2d`  | `--p2n`  | `--p2x`  |            | `--p2m`      |
+| In \\ out  | **CSV** | **TSV** | **JSON** | **JSONL** | **DKVP** | **NIDX** | **XTAB** | **PPRINT** | **Markdown** | **YAML** |
+|------------|---------|---------|----------|-----------|----------|----------|----------|------------|--------------|----------|
+| **CSV**    |         | `--c2t` | `--c2j`  | `--c2l`   | `--c2d`  | `--c2n`  | `--c2x`  | `--c2p`    | `--c2m`      | `--c2y`  |
+| **TSV**    | `--t2c` |         | `--t2j`  | `--t2l`   | `--t2d`  | `--t2n`  | `--t2x`  | `--t2p`    | `--t2m`      | `--t2y`  |
+| **JSON**   | `--j2c` | `--j2t` |          | `--j2l`   | `--j2d`  | `--j2n`  | `--j2x`  | `--j2p`    | `--j2m`      | `--j2y`  |
+| **JSONL**  | `--l2c` | `--l2t` | `--l2j`  |           | `--l2d`  | `--l2n`  | `--l2x`  | `--l2p`    | `--l2m`      | `--l2y`  |
+| **DKVP**   | `--d2c` | `--d2t` | `--d2j`  | `--d2l`   |          | `--d2n`  | `--d2x`  | `--d2p`    | `--d2m`      | `--d2y`  |
+| **NIDX**   | `--n2c` | `--n2t` | `--n2j`  | `--n2l`   | `--n2d`  |          | `--n2x`  | `--n2p`    | `--n2m`      | `--n2y`  |
+| **XTAB**   | `--x2c` | `--x2t` | `--x2j`  | `--x2l`   | `--x2d`  | `--x2n`  |          | `--x2p`    | `--x2m`      | `--x2y`  |
+| **PPRINT** | `--p2c` | `--p2t` | `--p2j`  | `--p2l`   | `--p2d`  | `--p2n`  | `--p2x`  |            | `--p2m`      | `--p2y`  |
+| **Markdown** | `--m2c` | `--m2t` | `--m2j`  | `--m2l`   | `--m2d`  | `--m2n`  | `--m2x`  | `--m2p`    |              | `--m2y`  |
+| **YAML**   | `--y2c` | `--y2t` | `--y2j`  | `--y2l`   | `--y2d`  | `--y2n`  | `--y2x`  | `--y2p`    | `--y2m`      | `--y2y`  |
 
 Additionally:
 
